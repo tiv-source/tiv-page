db8642542d47a7f85b37d140f07ff284ae44cd77
tiv-source <marc.michele@tiv-source.de>
2013-08-25 20:24:20 +0200

    Add first sub entity.

---
+++ entity/src/main/java/de/tivsource/page/entity/contentitem/Content.java
@@ -0,0 +1,101 @@
+package de.tivsource.page.entity.contentitem;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.TableGenerator;
+import javax.persistence.Temporal;
+
+import de.tivsource.page.entity.enumeration.Language;
+
+@Entity
+public class Content {
+
+    /**
+     * Datenbank-ID des Item-Objektes.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "content_generator")
+    @TableGenerator(name = "content_generator", initialValue = 0, allocationSize = 1)
+    @Column(name = "content_id")
+    private Long id;
+
+    @Lob
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private Language language;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private ContentItem contentItem;
+
+    /**
+     * Erfassungsdatum, Datum/Zeit an dem das Objekt in die Datenbank
+     * gespeichert wurde.
+     */
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date created;
+
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date modified;
+
+    public Long getId() {
+	return id;
+    }
+
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    public String getContent() {
+	return content;
+    }
+
+    public void setContent(String content) {
+	this.content = content;
+    }
+
+    public Language getLanguage() {
+	return language;
+    }
+
+    public void setLanguage(Language language) {
+	this.language = language;
+    }
+
+    public ContentItem getContentItem() {
+	return contentItem;
+    }
+
+    public void setContentItem(ContentItem contentItem) {
+	this.contentItem = contentItem;
+    }
+
+    public Date getCreated() {
+	return created;
+    }
+
+    public void setCreated(Date created) {
+	this.created = created;
+    }
+
+    public Date getModified() {
+	return modified;
+    }
+
+    public void setModified(Date modified) {
+	this.modified = modified;
+    }
+
+}// Ende class
---
+++ entity/src/main/java/de/tivsource/page/entity/contentitem/ContentItem.java
@@ -0,0 +1,63 @@
+package de.tivsource.page.entity.contentitem;
+
+import java.util.Map;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MapKey;
+import javax.persistence.OneToMany;
+
+import de.tivsource.page.entity.enumeration.Language;
+import de.tivsource.page.entity.namingitem.NamingItem;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class ContentItem extends NamingItem {
+
+    /**
+     * Die Map mit dem Inhalt des Objektes, die Angabe ist Lokalisiert.
+     */
+    @OneToMany(mappedBy = "contentItem", cascade = { CascadeType.ALL }, fetch = FetchType.EAGER)
+    @MapKey(name = "language")
+    private Map<Language, Content> contentMap;
+
+    public Map<Language, Content> getContentMap() {
+	return contentMap;
+    }
+
+    public void setContentMap(Map<Language, Content> contentMap) {
+	this.contentMap = contentMap;
+    }
+
+    public String getContent(String language) {
+	String result = contentMap.get(Language.DE).getContent();
+	String tmpResult = contentMap.get(Language.DE).getContent();
+	try {
+	    tmpResult = contentMap
+		    .get(Language.valueOf(language.toUpperCase())).getContent();
+	} catch (IllegalArgumentException e) {
+	    return result;
+	} catch (NullPointerException e) {
+	    return result;
+	}
+	return tmpResult;
+    }
+
+    public String getContent(Language language) {
+	String result = contentMap.get(Language.DE).getContent();
+	String tmpResult = contentMap.get(Language.DE).getContent();
+
+	try {
+	    tmpResult = contentMap.get(language).getContent();
+	} catch (IllegalArgumentException e) {
+	    return result;
+	} catch (NullPointerException e) {
+	    return result;
+	}
+	return tmpResult;
+    }
+
+}// Ende class
