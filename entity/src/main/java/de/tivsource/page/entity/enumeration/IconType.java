a9221c92b2374c9939e5c0b2d02fa1b5be8a85b6
tiv-source <marc.michele@tiv-source.de>
2013-08-26 09:41:32 +0200

    Add enumeration required by news entity.

---
+++ entity/src/main/java/de/tivsource/page/entity/enumeration/IconType.java
@@ -0,0 +1,19 @@
+package de.tivsource.page.entity.enumeration;
+
+/**
+ * @author Marc Michele
+ *
+ */
+public enum IconType {
+
+	CONSTRUCTION,
+	AIRPLANE,
+	QUESTIONMARK,
+	FOOTBALL,
+	ROCKET,
+	BRAIN,
+	EXCLAMATIONMARK,
+	CROSS,
+	TORSO;
+
+}// Ende enum
