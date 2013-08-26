6b422d3ce07583ecc87f29b4a5925104523d1723
tiv-source <marc.michele@tiv-source.de>
2013-08-26 09:26:11 +0200

    Add enumerations required by exhibition entity.

---
+++ entity/src/main/java/de/tivsource/page/entity/enumeration/CityType.java
@@ -0,0 +1,17 @@
+package de.tivsource.page.entity.enumeration;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public enum CityType {
+
+	BRUSSELS,
+	DORTMUND,
+	GDANSK,
+	MINDEN,
+	GRUENBERG,
+	UNSPECIFIED;
+	
+}// Ende enum
---
+++ entity/src/main/java/de/tivsource/page/entity/enumeration/CountryType.java
@@ -0,0 +1,15 @@
+package de.tivsource.page.entity.enumeration;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public enum CountryType {
+
+	GERMANY,
+	POLAND,
+	BELGIUM,
+	UNSPECIFIED;
+
+}// Ende enum
