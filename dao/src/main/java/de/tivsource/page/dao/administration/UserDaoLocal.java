65c2ffb34f943e7a26ee9b9ceab27542faba6243
tiv-source <marc.michele@tiv-source.de>
2015-02-15 15:55:08 +0100

    Add interface for User class to dao project.

---
+++ dao/src/main/java/de/tivsource/page/dao/administration/UserDaoLocal.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package de.tivsource.page.dao.administration;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import de.tivsource.page.entity.administration.User;
+
+/**
+ * @author Marc Michele
+ *
+ */
+@Local
+public interface UserDaoLocal {
+
+    /**
+     * Methode zum speichern eines Objektes der Klasse User.
+     * @param user - User Objekt das gespeichert werden soll
+     */
+    public void save(User user);
+
+    /**
+     * Methode zum verändern eines Objektes der Klasse User.
+     * @param user - User Objekt das verändert werden soll
+     */
+    public void merge(User user);
+
+    /**
+     * Methode zum löschen eines Objektes der Klasse User.
+     * @param user - zu löschendes User Objekt
+     */
+    public void delete(User user);
+
+    /**
+     * Methode zum laden einer Liste von User Objekten, es muss dabei ein
+     * Startwert angegeben werden und die Anzhal der zu ladenen User Objekte.
+     *
+     * @param start - Startwert ab der die Liste beginnen soll
+     * @param max - Maximale Anzahl an Objekten die die Liste enthalten soll
+     * @return List<User> - Liste von User Objekten
+     */
+    public List<User> findAll(Integer start, Integer max);
+
+    /**
+     * Methode die die Anzahl aller User Objekte die sich in der Datenbank
+     * befinden zurück liefert.
+     *
+     * @return Integer - Anzahl der User Objekte die sich in der Datenbank befinden.
+     */
+    public Integer countAll();
+
+}// Ende interface
