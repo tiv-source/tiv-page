8f229c5e69cd5d42f7b8460f5730ee12d45fd2a6
tiv-source <marc.michele@tiv-source.de>
2015-03-02 21:01:27 +0100

    Add form action of Role class to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/role/FormAction.java
@@ -0,0 +1,88 @@
+package de.tivsource.page.admin.actions.role;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.ejb3plugin.InjectEJB;
+import de.tivsource.page.admin.actions.EmptyAction;
+import de.tivsource.page.dao.administration.RoleDaoLocal;
+import de.tivsource.page.dao.administration.UserDaoLocal;
+import de.tivsource.page.entity.administration.Role;
+import de.tivsource.page.entity.administration.User;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class FormAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+	private static final long serialVersionUID = 7303549694077697578L;
+
+	/**
+	 * Statischer Logger der Klasse.
+	 */
+	private static final Logger LOGGER = Logger.getLogger("INFO");
+
+    @InjectEJB(name="RoleDao")
+    private RoleDaoLocal roleDaoLocal;
+	
+	@InjectEJB(name="UserDao")
+    private UserDaoLocal userDaoLocal;
+
+	private Role role;
+
+	private String uncheckRole;
+
+	public Role getRole() {
+        return role;
+    }
+
+	public void setRole(String uncheckRole) {
+        this.uncheckRole = uncheckRole;
+    }
+
+	@Override
+    @Actions({
+        @Action(
+        		value = "editForm", 
+        		results = { @Result(name = "success", type="tiles", location = "roleEditForm") }
+        ),
+        @Action(
+        		value = "addForm", 
+        		results = { @Result(name = "success", type="tiles", location = "roleAddForm") }
+        ),
+        @Action(
+        		value = "deleteForm", 
+        		results = { @Result(name = "success", type="tiles", location = "roleDeleteForm") }
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+    	
+    	this.loadPageParameter();
+    	return SUCCESS;
+    }// Ende execute()
+
+	public List<User> getUserList() {
+	    return userDaoLocal.findAll(0, userDaoLocal.countAll());
+	}
+	
+	private void loadPageParameter() {
+
+		if( uncheckRole != null && uncheckRole != "" && uncheckRole.length() > 0) {
+		    role = roleDaoLocal.findByUuid(uncheckRole);
+		} else {
+		    role = new Role();
+		}
+
+	}// Ende loadPageParameter()
+	
+}// Ende class
