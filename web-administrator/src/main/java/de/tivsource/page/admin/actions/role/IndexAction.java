e5e934f960f03d56b11e40f5dbbd375a3ce1001e
tiv-source <marc.michele@tiv-source.de>
2015-03-02 21:01:42 +0100

    Add default action of Role class to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/role/IndexAction.java
@@ -0,0 +1,40 @@
+package de.tivsource.page.admin.actions.role;
+
+import java.util.logging.Logger;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.page.admin.actions.EmptyAction;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class IndexAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+	private static final long serialVersionUID = 2134111727766295415L;
+
+	/**
+	 * Statischer Logger der Klasse.
+	 */
+	private static final Logger LOGGER = Logger.getLogger("INFO");
+
+	@Override
+    @Actions({
+        @Action(
+        		value = "index", 
+        		results = { @Result(name = "success", type="tiles", location = "role") }
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+    	return SUCCESS;
+    }// Ende execute()
+
+}// Ende class
