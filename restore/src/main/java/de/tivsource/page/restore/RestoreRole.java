06f7c7ed1ee4c3ab5856cce86707b745fa208040
tiv-source <marc.michele@tiv-source.de>
2015-06-17 17:52:18 +0200

    Add restore class for Role entity to restore module.

---
+++ restore/src/main/java/de/tivsource/page/restore/RestoreRole.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package de.tivsource.page.restore;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.logging.Logger;
+
+import de.tivsource.page.dao.administration.RoleDaoLocal;
+import de.tivsource.page.entity.administration.Role;
+
+/**
+ * @author Marc Michele
+ *
+ */
+public class RestoreRole {
+
+    private static final Logger LOGGER = Logger.getLogger("INFO");
+
+    private RoleDaoLocal roleDaoLocal;
+
+    public RestoreRole(RoleDaoLocal roleDaoLocal) {
+        super();
+        this.roleDaoLocal = roleDaoLocal;
+    }
+
+    public void generate(InputStream inputStream) {
+        LOGGER.info("generate(InputStream inputStream) aufgerufen.");
+        cleanup();
+        BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                if (!line.startsWith("[Format Definition]")) {
+                    Role role = convert(line);
+                    roleDaoLocal.merge(role);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }// Ende generate()
+
+    private Role convert(String line) {
+        // Zerlege CSV-Zeile in String-Array.
+        String[] items = line.split("\\|");
+
+        Role role = new Role();
+        role.setUuid(items[0]);
+        role.setTechnical(items[1]);
+        role.setCreated(convertDateString(items[2]));
+        role.setModified(convertDateString(items[3]));
+        role.setModifiedBy(items[4]);
+        role.setIp(items[5]);
+
+        return role;
+    }
+
+    /**
+     * Methode zum Konvertieren eines Strings des Formates "1970-12-01 23:59:59" in ein Date-Object. 
+     * @param dateString
+     * @return
+     */
+    private Date convertDateString(String dateString) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            return simpleDateFormat.parse(dateString);
+        } catch (ParseException e) {
+            return new Date();
+        }
+    }// Ende convertDateString(String dateString)
+
+    private void cleanup() {
+        if(roleDaoLocal.countAll() > 0) {
+            Iterator<Role> roleIterator = roleDaoLocal.findAll(0, roleDaoLocal.countAll()).iterator();
+            while(roleIterator.hasNext()) {
+                Role next = roleIterator.next();
+                next.setUsers(null);
+                roleDaoLocal.merge(next);
+                roleDaoLocal.delete(next);
+            }
+        }
+    }// Ende cleanup()
+    
+}// Ende class
