22ba0a63cf3fe0519fb1c5d15968ee0d5b191e11
tiv-source <marc.michele@tiv-source.de>
2015-06-18 13:06:48 +0200

    Add code to determine remote user and remote address. Also add new serialVersionUID to edit action of Page class in web-administrator module.

--- web-administrator/src/main/java/de/tivsource/page/admin/actions/page/EditAction.java
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/page/EditAction.java
@@ -3,6 +3,7 @@ package de.tivsource.page.admin.actions.page;
 import java.util.Date;
 import java.util.logging.Logger;
 
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Actions;
 import org.apache.struts2.convention.annotation.Result;
@@ -23,9 +24,9 @@ public class EditAction extends EmptyAction {
 	/**
 	 * Serial Version UID.
 	 */
-	private static final long serialVersionUID = 2569545967755018736L;
+    private static final long serialVersionUID = 7907625857484921700L;
 
-	/**
+    /**
 	 * Statischer Logger der Klasse.
 	 */
 	private static final Logger LOGGER = Logger.getLogger("INFO");
@@ -35,12 +36,22 @@ public class EditAction extends EmptyAction {
 
     private Page page;
 
+    private String lang;
+
     public Page getPage() {
         return page;
     }
 
 	public void setPage(Page page) {
         this.page = page;
+    }
+	
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
     }
 
     @Override
@@ -56,22 +67,46 @@ public class EditAction extends EmptyAction {
     })
     public String execute() throws Exception {
     	LOGGER.info("execute() aufgerufen.");
-    	
+
+        String remoteUser    = ServletActionContext.getRequest().getRemoteUser();
+        String remoteAddress = ServletActionContext.getRequest().getRemoteAddr();
+
     	if(page != null) {
     		LOGGER.info(page.getTechnical());
     		Page dbPage = pageDaoLocal.findByUuid(page.getUuid());
     		
-    		dbPage.getContentMap().get(Language.DE).setContent(page.getContent(Language.DE));
-    		dbPage.getContentMap().get(Language.DE).setModified(new Date());
-    		
-    		dbPage.getDescriptionMap().get(Language.DE).setDescription(page.getDescription(Language.DE));
-    		dbPage.getDescriptionMap().get(Language.DE).setKeywords(page.getKeywords(Language.DE));;
-    		dbPage.getDescriptionMap().get(Language.DE).setName(page.getName(Language.DE));
-    		
+
+            if(lang.contentEquals(new StringBuffer("EN"))) {
+                page.getContentMap().put(Language.DE, dbPage.getContentObject(Language.DE));
+                dbPage.getContentMap().get(Language.EN).setContent(page.getContent(Language.EN));
+                dbPage.getContentMap().get(Language.EN).setModified(new Date());
+
+                page.getDescriptionMap().put(Language.DE, dbPage.getDescriptionObject(Language.DE));
+                dbPage.getDescriptionMap().get(Language.EN).setDescription(page.getDescription(Language.EN));
+                dbPage.getDescriptionMap().get(Language.EN).setKeywords(page.getKeywords(Language.EN));
+                dbPage.getDescriptionMap().get(Language.EN).setName(page.getName(Language.EN));
+            } else {
+                dbPage.getContentMap().get(Language.DE).setContent(page.getContent(Language.DE));
+                dbPage.getContentMap().get(Language.DE).setModified(new Date());
+
+                dbPage.getDescriptionMap().get(Language.DE).setDescription(page.getDescription(Language.DE));
+                dbPage.getDescriptionMap().get(Language.DE).setKeywords(page.getKeywords(Language.DE));;
+                dbPage.getDescriptionMap().get(Language.DE).setName(page.getName(Language.DE));
+            }
+
+
+            dbPage.setBottomNavigation(page.getBottomNavigation());
+            dbPage.setBottomNavigationOrder(page.getBottomNavigationOrder());
     		dbPage.setModified(new Date());
+    		dbPage.setNavigation(page.getNavigation());
+    		dbPage.setNavigationOrder(page.getNavigationOrder());
     		dbPage.setSpecial(page.getSpecial());
     		dbPage.setTechnical(page.getTechnical());
+    		dbPage.setTopNavigation(page.getTopNavigation());
+    		dbPage.setTopNavigationOrder(page.getTopNavigationOrder());
     		dbPage.setVisible(page.getVisible());
+    		dbPage.setModifiedBy(remoteUser);
+    		dbPage.setIp(remoteAddress);
 
     		pageDaoLocal.merge(dbPage);
             return SUCCESS;
