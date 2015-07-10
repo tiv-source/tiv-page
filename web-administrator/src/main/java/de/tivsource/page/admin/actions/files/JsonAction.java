b2f8d0cae75d6721d97dc66d22cd832623c3d7a8
tiv-source <marc.michele@tiv-source.de>
2015-07-10 22:55:47 +0200

    Add json action for files package.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/files/JsonAction.java
@@ -0,0 +1,145 @@
+package de.tivsource.page.admin.actions.files;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author Marc Michele
+ * 
+ */
+@ParentPackage(value = "administratorJson")
+public class JsonAction extends ActionSupport {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 3264469345260180786L;
+
+    private static final Logger logger = Logger.getLogger("INFO");
+
+    private File folder = new File("/srv/www/htdocs/uploads");
+
+    private File[] rawFileArray = folder.listFiles();
+
+    private List<File> fileList = new ArrayList<File>();
+    
+    private List<File> gridModel;
+
+    private Integer rows = 0;
+    private Integer page = 1;
+    private Integer total = 0;
+    private Integer record = 0;
+    private String sord;
+    private String sidx;
+
+    @Override
+    @Actions({
+        @Action(
+                value = "table", 
+                results = { @Result(name = "success", type="json", params={"excludeProperties", "gridModel.*.absolute, gridModel.*.absoluteFile, gridModel.*.absolutePath, gridModel.*.canonicalFile, gridModel.*.canonicalPath, gridModel.*.directory, gridModel.*.file, gridModel.*.freeSpace, gridModel.*.parentFile, gridModel.*.path, gridModel.*.totalSpace, gridModel.*.usableSpace, gridModel.*.parent"}) }
+        )
+    })
+    public String execute() {
+        return SUCCESS;
+    }
+
+    public String getJSON() {
+        logger.info("Page " + getPage() + " Rows " + getRows()
+                + " Sorting Order " + getSord() + " Index Row :" + getSidx());
+        logger.info("Build new List");
+
+        /*
+        for (int i = 0; i < rawFileArray.length; i++) {
+            if (rawFileArray[i].isFile()) {
+                fileList.add(rawFileArray[i]);
+            }
+        }
+        */
+
+        fileList = Arrays.asList(rawFileArray);
+
+        setRecord(this.fileList.size());
+
+        
+        
+        setTotal((int) Math.ceil((double) getRecord() / (double) getRows()));
+
+        setGridModel(fileList);
+
+        logger.info("Rows:" + rows);
+        logger.info("Page:" + page);
+        logger.info("Total:" + total);
+        logger.info("Record:" + record);
+        logger.info("Sord:" + sord);
+        logger.info("Sidx:" + sidx);
+        
+        return execute();
+    }
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Integer getRecord() {
+        return record;
+    }
+
+    public void setRecord(Integer record) {
+        this.record = record;
+    }
+
+    public List<File> getGridModel() {
+        return gridModel;
+    }
+
+    public void setGridModel(List<File> gridModel) {
+        this.gridModel = gridModel;
+    }
+
+    public String getSord() {
+        return sord;
+    }
+
+    public void setSord(String sord) {
+        this.sord = sord;
+    }
+
+    public String getSidx() {
+        return sidx;
+    }
+
+    public void setSidx(String sidx) {
+        this.sidx = sidx;
+    }
+    
+}// Ende class
