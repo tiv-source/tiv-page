3323c0512ae1b83ffcefa9b6027f58f78f8f4087
tiv-source <marc.michele@tiv-source.de>
2015-07-10 22:55:08 +0200

    Add form action for files package.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/files/FormAction.java
@@ -0,0 +1,54 @@
+package de.tivsource.page.admin.actions.files;
+
+import java.util.logging.Logger;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.page.admin.actions.EmptyAction;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class FormAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+    private static final long serialVersionUID = 5016294531938889095L;
+
+    /**
+	 * Statischer Logger der Klasse.
+	 */
+    private static final Logger LOGGER = Logger.getLogger("INFO");
+
+    private String file;
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    @Override
+    @Actions({
+        @Action(
+        		value = "addForm", 
+        		results = { @Result(name = "success", type="tiles", location = "filesAddForm") }
+        ),
+        @Action(
+        		value = "deleteForm", 
+        		results = { @Result(name = "success", type="tiles", location = "filesDeleteForm") }
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+    	return SUCCESS;
+    }// Ende execute()
+
+}// Ende class
