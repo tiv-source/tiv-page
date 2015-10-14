ad674fd34f9f9859b7677c5dc85529342d08a7e3
tiv-source <marc.michele@tiv-source.de>
2015-10-14 20:17:10 +0200

    Add dao interface for Property class to dao project.

---
+++ dao/src/main/java/de/tivsource/page/dao/property/PropertyDaoLocal.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package de.tivsource.page.dao.property;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import de.tivsource.page.entity.property.Property;
+
+/**
+ * @author Marc Michele
+ *
+ */
+@Local
+public interface PropertyDaoLocal {
+
+    /**
+     * Methode zum speichern eines Objektes der Klasse Property.
+     * @param property - Property Objekt das gespeichert werden soll
+     */
+    public void save(Property property);
+
+    /**
+     * Methode zum verändern eines Objektes der Klasse Property.
+     * @param property - Property Objekt das verändert werden soll
+     */
+    public void merge(Property property);
+
+    /**
+     * Methode zum löschen eines Objektes der Klasse Property.
+     * @param property - zu löschendes Property Objekt
+     */
+    public void delete(Property property);
+
+    public Property findByKey(String key);
+
+    /**
+     * Methode zum laden einer Liste von Property Objekten, es muss dabei ein
+     * Startwert angegeben werden und die Anzhal der zu ladenen Property Objekte.
+     *
+     * @param start - Startwert ab der die Liste beginnen soll
+     * @param max - Maximale Anzahl an Objekten die die Liste enthalten soll
+     * @return List<Property> - Liste von Property Objekten
+     */
+    public List<Property> findAll(Integer start, Integer max);
+
+    public List<Property> findAll(Integer start, Integer max, String field, String order);
+
+    /**
+     * Methode die die Anzahl aller Property Objekte die sich in der Datenbank
+     * befinden zurück liefert.
+     *
+     * @return Integer - Anzahl der Property Objekte die sich in der Datenbank befinden.
+     */
+    public Integer countAll();
+
+}// Ende interface
