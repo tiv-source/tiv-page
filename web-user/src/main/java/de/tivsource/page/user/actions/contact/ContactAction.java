51f3db57644b0fea121b5bb74d245db65353c9ce
tiv-source <marc.michele@tiv-source.de>
2015-10-17 20:48:47 +0200

    Add action for contact form to web-user project.

---
+++ web-user/src/main/java/de/tivsource/page/user/actions/contact/ContactAction.java
@@ -0,0 +1,68 @@
+package de.tivsource.page.user.actions.contact;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.ejb3plugin.InjectEJB;
+import de.tivsource.page.dao.page.PageDaoLocal;
+import de.tivsource.page.dao.property.PropertyDaoLocal;
+import de.tivsource.page.entity.page.Page;
+import de.tivsource.page.user.actions.EmptyAction;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class ContactAction extends EmptyAction {
+
+    /**
+     * Serial Version UID.
+     */
+    private static final long serialVersionUID = 8491900732546714341L;
+
+    /**
+	 * Statischer Logger der Klasse.
+	 */
+    private static final Logger LOGGER = Logger.getLogger(ContactAction.class);
+
+    @InjectEJB(name="PageDao")
+    private PageDaoLocal pageDaoLocal;
+
+    @InjectEJB(name="PropertyDao")
+    private PropertyDaoLocal propertyDaoLocal;
+
+    @Override
+    @Actions({
+        @Action(
+        		value = "index", 
+        		results = {
+        		  @Result(name = "success", type="tiles", location = "contact"),
+        		  @Result(name = "error", type = "redirectAction", location = "index.html", params={"namespace", "/"})
+        		}
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+    	
+    	// Hole Action Locale
+    	this.getLanguageFromActionContext();
+
+    	boolean contactPageEnabled = propertyDaoLocal.findByKey("contact.page.enabled").getValue().equals("true") ? true : false;
+
+    	if(contactPageEnabled) {
+            LOGGER.info("Pfad zur temp.xml Klasse: " + this.getClass().getClassLoader().getResource("/temp.xml"));
+            return SUCCESS;
+    	} else {
+    	    return ERROR;
+    	}
+
+    }// Ende execute()
+
+    public Page getPage() {
+    	return pageDaoLocal.findByTechnical("contact");
+    }// Ende getPage()
+
+}// Ende class
