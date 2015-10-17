713fcf1a8975e13a8218b1090e989aef86630e6e
tiv-source <marc.michele@tiv-source.de>
2015-10-17 14:37:32 +0200

    Add embeddable class Address to entity project.

---
+++ entity/src/main/java/de/tivsource/page/entity/location/Address.java
@@ -0,0 +1,65 @@
+package de.tivsource.page.entity.location;
+
+import javax.persistence.Embeddable;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+@Embeddable
+public class Address {
+
+    /**
+     * Strasse
+     */
+    private String street;
+
+    /**
+     * Postleitzahl
+     */
+    private String zip;
+
+    /**
+     * Stadt
+     */
+    private String city;
+
+    /**
+     * Land
+     */
+    private String country;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+}// Ende class
