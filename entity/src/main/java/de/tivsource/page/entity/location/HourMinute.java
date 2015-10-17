9a9d4cb2eb9af729b8b510f481c83f18fc201750
tiv-source <marc.michele@tiv-source.de>
2015-10-17 14:38:51 +0200

    Add embeddable class HourMinute to entity project.

---
+++ entity/src/main/java/de/tivsource/page/entity/location/HourMinute.java
@@ -0,0 +1,89 @@
+package de.tivsource.page.entity.location;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+@Embeddable
+public class HourMinute implements Comparable<HourMinute> {
+
+	@Column(length=2)
+	private Integer hour;
+
+	@Column(length=2)
+	private Integer minute;
+
+	public Integer getHour() {
+		return hour;
+	}
+
+	public void setHour(Integer hour) {
+		this.hour = hour;
+	}
+
+	public Integer getMinute() {
+		return minute;
+	}
+
+	public void setMinute(Integer minute) {
+		this.minute = minute;
+	}
+
+    @Override
+    public int compareTo(HourMinute o) {
+        // Wenn die Stunde des übergebenen Objektes kleiner ist
+        if(o.hour < hour) {
+            return 1;
+        }
+        // Wenn die Stunde des übergebenen Objektes gößer ist
+        if(o.hour > hour) {
+            return -1;
+        }
+        // Wenn die Minuten des übergebenen Objektes kleiner sind
+        if(o.minute < minute) {
+            return 1;
+        }
+        // Wenn die Minuten des übergebenen Objektes größer sind
+        if(o.minute > minute) {
+            return -1;
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // Wenn die Objekte gleich sind.
+        if(this == obj) {
+            return true;
+        }
+        // Wenn das Objekt null ist.
+        if(obj == null) {
+            return false;
+        }
+        // Wenn es sich nicht um die gleich Klasse handelt.
+        if (!(obj instanceof OpeningHour)) {
+            return false;
+        }
+        // Objekt Casting
+        HourMinute other = (HourMinute) obj;
+        // Wenn der Inhalt Identisch ist dann ist das Objekt gleich.
+        if(this.hour.equals(other.hour) &&
+           this.minute.equals(other.minute)){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 1;
+        hash = hash * 7 * (hour == null ? 0 : hour.hashCode());
+        hash = hash * 7 * (minute == null ? 0 : minute.hashCode());
+        return hash;
+    }
+
+}// End class
