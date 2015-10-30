bcc445fe14fdb99993c087c2eb96872bf018951c
tiv-source <marc.michele@tiv-source.de>
2015-10-30 21:43:19 +0100

    Complete source of Event class.

--- entity/src/main/java/de/tivsource/page/entity/event/Event.java
+++ entity/src/main/java/de/tivsource/page/entity/event/Event.java
@@ -5,13 +5,22 @@ package de.tivsource.page.entity.event;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 
+import de.tivsource.page.entity.location.Location;
 import de.tivsource.page.entity.namingitem.NamingItem;
+import de.tivsource.page.entity.reservation.Reservation;
 
 /**
  * @author Marc Michele
@@ -29,6 +38,22 @@ public class Event extends NamingItem {
     @Basic
     @org.hibernate.annotations.Type(type = "yes_no")
     private Boolean supper;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "location_uuid")
+    private Location location;
+
+    @Basic
+    @org.hibernate.annotations.Type(type = "yes_no")
+    private Boolean reservation;
+
+    @ManyToMany(targetEntity = Reservation.class, cascade = { CascadeType.PERSIST }, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "Event_Reservation", 
+            joinColumns = @JoinColumn(name = "event_uuid"), 
+            inverseJoinColumns = @JoinColumn(name = "reservation_uuid")
+            )
+    private List<Reservation> reservations;
 
     public BigDecimal getPrice() {
         return price;
@@ -52,6 +77,22 @@ public class Event extends NamingItem {
 
     public void setSupper(Boolean supper) {
         this.supper = supper;
+    }
+
+    public Boolean getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Boolean reservation) {
+        this.reservation = reservation;
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
     }
 
 }// Ende class
