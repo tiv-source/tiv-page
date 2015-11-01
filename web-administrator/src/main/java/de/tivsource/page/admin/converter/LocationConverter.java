673aa50a13bf288ae165eec2f916338869734f67
tiv-source <marc.michele@tiv-source.de>
2015-11-01 15:52:59 +0100

    Add converter for Location class to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/converter/LocationConverter.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package de.tivsource.page.admin.converter;
+
+import java.util.Map;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.util.StrutsTypeConverter;
+
+import com.opensymphony.xwork2.conversion.TypeConversionException;
+
+import de.tivsource.page.dao.location.LocationDaoLocal;
+import de.tivsource.page.entity.location.Location;
+
+/**
+ * @author marc
+ *
+ */
+public class LocationConverter extends StrutsTypeConverter {
+
+    /**
+     * Statischer Logger der Klasse.
+     */
+    private static final Logger LOGGER = Logger.getLogger(LocationConverter.class);
+
+	/* (non-Javadoc)
+	 * @see org.apache.struts2.util.StrutsTypeConverter#convertFromString(java.util.Map, java.lang.String[], java.lang.Class)
+	 */
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Object convertFromString(Map context, String[] values, Class toClass) {
+
+        if (values == null || values.length == 0 || values[0].trim().length() == 0) {
+        	LOGGER.info("Keine Location UUID angegeben.");
+        	throw new TypeConversionException("Keine Location UUID angegeben angegeben.");
+        }
+        
+        try {
+            Context initialContext = new InitialContext();
+            // TODO: Remove version number from dao
+            LocationDaoLocal locationDaoLocal = (LocationDaoLocal) initialContext.lookup("java:global/tiv-page/dao-0.0.1/LocationDao");
+            return locationDaoLocal.findByUuid(values[0]);
+        } catch (Exception e) {
+        	LOGGER.info("Keine gültige Location UUID angegeben.");
+        	throw new TypeConversionException("Keine gültige Location UUID angegeben.");
+        }
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.struts2.util.StrutsTypeConverter#convertToString(java.util.Map, java.lang.Object)
+	 */
+	@SuppressWarnings("rawtypes")
+	@Override
+	public String convertToString(Map context, Object o) {
+		Location location = (Location)o;
+		return location.getUuid();
+	}
+
+}// Ende class
