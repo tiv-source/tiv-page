d1f9e09956d9bbc113c52b45f3235f5f1836b762
tiv-source <marc.michele@tiv-source.de>
2015-12-04 10:31:19 +0100

    Add EmailTemplate class to entity project.

---
+++ entity/src/main/java/de/tivsource/page/helper/EmailTemplate.java
@@ -0,0 +1,188 @@
+package de.tivsource.page.helper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.mail.internet.InternetAddress;
+
+import org.apache.commons.digester.Digester;
+import org.apache.log4j.Logger;
+import org.xml.sax.SAXException;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class EmailTemplate {
+
+    /**
+     * Statischer Logger der Klasse EmailTemplate, zur Zeit gibt es Meldungen
+     * vom Type TRACE und DEBUG.
+     */
+    private static final Logger LOGGER = Logger.getLogger(EmailTemplate.class);
+
+    private String subject;
+    private String body;
+    private String from;
+    private String html;
+
+    private ArrayList<String> to = new ArrayList<String>();
+    private ArrayList<String> cc = new ArrayList<String>();
+    private ArrayList<String> bcc = new ArrayList<String>();
+
+    public String getHtml() {
+        return html;
+    }
+
+    public void setHtml(String html) {
+        this.html = html;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public ArrayList<String> getTo() {
+        return to;
+    }
+
+    public void setTo(ArrayList<String> to) {
+        this.to = to;
+    }
+
+    public ArrayList<String> getCc() {
+        return cc;
+    }
+
+    public void setCc(ArrayList<String> cc) {
+        this.cc = cc;
+    }
+
+    public ArrayList<String> getBcc() {
+        return bcc;
+    }
+
+    public void setBcc(ArrayList<String> bcc) {
+        this.bcc = bcc;
+    }
+
+    public void addTo(String toSet) {
+        to.add(toSet);
+    }
+
+    public void addCc(String ccSet) {
+        cc.add(ccSet);
+    }
+
+    public void addBcc(String bccSet) {
+        bcc.add(bccSet);
+    }
+
+    public InternetAddress[] getToAddresses() {
+
+        InternetAddress[] summary = new InternetAddress[to.size()];
+
+        int counter = 0;
+        Iterator<String> toIt = to.iterator();
+
+        while (toIt.hasNext()) {
+            InternetAddress iaddress = new InternetAddress();
+            String next = toIt.next();
+            iaddress.setAddress(next);
+            summary[counter] = iaddress;
+            counter++;
+        }
+
+        return summary;
+    }
+
+    public InternetAddress[] getCcAddresses() {
+
+        InternetAddress[] summary = new InternetAddress[cc.size()];
+
+        int counter = 0;
+        Iterator<String> ccIt = cc.iterator();
+
+        while (ccIt.hasNext()) {
+            InternetAddress iaddress = new InternetAddress();
+            iaddress.setAddress(ccIt.next());
+            summary[counter] = iaddress;
+            counter++;
+        }
+
+        return summary;
+    }
+
+    public InternetAddress[] getBccAddresses() {
+        InternetAddress[] summary = new InternetAddress[bcc.size()];
+
+        int counter = 0;
+        Iterator<String> bccIt = bcc.iterator();
+
+        while (bccIt.hasNext()) {
+            InternetAddress iaddress = new InternetAddress();
+            iaddress.setAddress(bccIt.next());
+            summary[counter] = iaddress;
+            counter++;
+        }
+
+        return summary;
+    }
+
+    public static Object getEmailTemplate(InputStream aStream) {
+    	System.out.println("getEmailTemplate(InputStream aStream) aufgerufen");
+    	Digester digester = new Digester();
+        digester.setValidating(false);
+        digester.setUseContextClassLoader(true);
+        System.out.println("Digester gesetzt 0");
+
+        digester.addObjectCreate("email", EmailTemplate.class);
+
+        digester.addBeanPropertySetter("email/subject", "subject");
+        digester.addBeanPropertySetter("email/body", "body");
+        digester.addBeanPropertySetter("email/from", "from");
+        digester.addBeanPropertySetter("email/html", "html");
+        digester.addCallMethod("email/to", "addTo", 0);
+        digester.addCallMethod("email/cc", "addCc", 0);
+        digester.addCallMethod("email/bcc", "addBcc", 0);
+        System.out.println("Digester gesetzt 1");
+        try {
+        	//File templateFile = new File("/home/marc/Server/apache-tomee-plus-1.5.2/apps/czastka/web-user-0.0.1/WEB-INF/classes/temp.xml");
+        	//System.out.println("Size: " + templateFile.getFreeSpace());
+        	//InputStream aStream2 = new FileInputStream(templateFile);
+        	System.out.println(aStream.available());
+            return digester.parse(aStream);
+        } catch (IOException e) {
+            LOGGER.info("Error: IOException");
+            e.printStackTrace();
+            return null;
+        } catch (SAXException e) {
+            LOGGER.info("Error: SAXException");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
