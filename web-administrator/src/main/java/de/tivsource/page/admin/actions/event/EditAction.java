f3ca0221dfea96a5451e7767dd5ca182bb3cbd6e
tiv-source <marc.michele@tiv-source.de>
2015-12-05 02:32:29 +0100

    Update edit action of Event class.

--- web-administrator/src/main/java/de/tivsource/page/admin/actions/event/EditAction.java
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/event/EditAction.java
@@ -1,6 +1,10 @@
 package de.tivsource.page.admin.actions.event;
 
+import java.util.Date;
+import java.util.List;
+
 import org.apache.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Actions;
 import org.apache.struts2.convention.annotation.Result;
@@ -8,7 +12,10 @@ import org.apache.struts2.convention.annotation.Result;
 import de.tivsource.ejb3plugin.InjectEJB;
 import de.tivsource.page.admin.actions.EmptyAction;
 import de.tivsource.page.dao.event.EventDaoLocal;
+import de.tivsource.page.dao.location.LocationDaoLocal;
+import de.tivsource.page.entity.enumeration.Language;
 import de.tivsource.page.entity.event.Event;
+import de.tivsource.page.entity.location.Location;
 
 /**
  * 
@@ -30,7 +37,12 @@ public class EditAction extends EmptyAction {
     @InjectEJB(name="EventDao")
     private EventDaoLocal eventDaoLocal;
 
+    @InjectEJB(name="LocationDao")
+    private LocationDaoLocal locationDaoLocal;
+
     private Event event;
+
+    private String lang;
 
     public Event getEvent() {
         return event;
@@ -38,6 +50,14 @@ public class EditAction extends EmptyAction {
 
     public void setEvent(Event event) {
         this.event = event;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
     }
 
     @Override
@@ -54,10 +74,36 @@ public class EditAction extends EmptyAction {
     public String execute() throws Exception {
     	LOGGER.info("execute() aufgerufen.");
 
+        String remoteUser    = ServletActionContext.getRequest().getRemoteUser();
+        String remoteAddress = ServletActionContext.getRequest().getRemoteAddr();
+    	
     	if(event != null) {
     		LOGGER.info("UUID des Events: " + event.getUuid());
     		Event dbEvent = eventDaoLocal.findByUuid(event.getUuid());
+
+            if(lang.contentEquals(new StringBuffer("EN"))) {
+                event.getDescriptionMap().put(Language.DE, dbEvent.getDescriptionObject(Language.DE));
+                dbEvent.getDescriptionMap().get(Language.EN).setDescription(event.getDescription(Language.EN));
+                dbEvent.getDescriptionMap().get(Language.EN).setKeywords(event.getKeywords(Language.EN));
+                dbEvent.getDescriptionMap().get(Language.EN).setName(event.getName(Language.EN));
+            } else {
+                dbEvent.getDescriptionMap().get(Language.DE).setDescription(event.getDescription(Language.DE));
+                dbEvent.getDescriptionMap().get(Language.DE).setKeywords(event.getKeywords(Language.DE));;
+                dbEvent.getDescriptionMap().get(Language.DE).setName(event.getName(Language.DE));
+            }
+    		
     		dbEvent.setBeginning(event.getBeginning());
+    		dbEvent.setDeadline(event.getDeadline());
+    		dbEvent.setEnding(event.getEnding());
+    		dbEvent.setIp(remoteAddress);
+    		dbEvent.setLocation(locationDaoLocal.findByUuidWidthEvents(event.getLocation().getUuid()));
+    		dbEvent.setModified(new Date());
+    		dbEvent.setModifiedBy(remoteUser);
+    		dbEvent.setPrice(event.getPrice());
+    		dbEvent.setReservation(event.getReservation());
+    		dbEvent.setVisible(event.getVisible());
+    		
+    		
     		eventDaoLocal.merge(dbEvent);
             return SUCCESS;
     	}
@@ -66,5 +112,9 @@ public class EditAction extends EmptyAction {
     	}
 
     }// Ende execute()
+
+    public List<Location> getLocationList() {
+        return locationDaoLocal.findAllEventLocation();
+    }
 
 }// Ende class
