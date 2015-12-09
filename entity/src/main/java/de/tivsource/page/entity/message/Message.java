022658532805b0c5ab25c87a2c114211617c64a2
tiv-source <marc.michele@tiv-source.de>
2015-12-09 11:28:57 +0100

    Add attributes to Message class.

--- entity/src/main/java/de/tivsource/page/entity/message/Message.java
+++ entity/src/main/java/de/tivsource/page/entity/message/Message.java
@@ -1,9 +1,16 @@
 package de.tivsource.page.entity.message;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.Temporal;
+
+import org.hibernate.search.annotations.DocumentId;
 
 import de.tivsource.page.enumeration.GenderType;
 
@@ -14,6 +21,16 @@ import de.tivsource.page.enumeration.GenderType;
  */
 @Entity
 public class Message {
+
+    /**
+     * UUID des Objektes der Klasse Message, diese ID ist einmalig über alle
+     * Objekte hinweg und sollte der bevorzugte weg sein auf bestimmte Objekte
+     * zuzugreifen.
+     */
+    @Id
+    @DocumentId
+    @Column(name="uuid", unique=true)
+    private String uuid;
 
 	/**
 	 * Geschlecht des Kontaktes.
@@ -43,7 +60,20 @@ public class Message {
 	@Lob
 	private String content;
 
-	public GenderType getGenderType() {
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date created;
+
+    private String ip;
+
+	public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public GenderType getGenderType() {
 		return genderType;
 	}
 
@@ -97,6 +127,22 @@ public class Message {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
     }
 
 }// Ende class
