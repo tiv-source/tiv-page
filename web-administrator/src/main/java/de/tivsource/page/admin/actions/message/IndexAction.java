ab8906d695562686e20ecd6fb04f398960e11aec
tiv-source <marc.michele@tiv-source.de>
2015-12-09 11:40:57 +0100

    Add index action for Message class to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/message/IndexAction.java
@@ -0,0 +1,39 @@
+package de.tivsource.page.admin.actions.message;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.page.admin.actions.EmptyAction;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class IndexAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+    private static final long serialVersionUID = 1768145629322201123L;
+
+    /**
+     * Statischer Logger der Klasse.
+     */
+    private static final Logger LOGGER = Logger.getLogger(IndexAction.class);
+
+	@Override
+    @Actions({
+        @Action(
+        		value = "index", 
+        		results = { @Result(name = "success", type="tiles", location = "message") }
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+    	return SUCCESS;
+    }// Ende execute()
+
+}// Ende class
