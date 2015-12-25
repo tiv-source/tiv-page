719923268e35f97346c9cda1d412d8f504628303
tiv-source <marc.michele@tiv-source.de>
2015-12-25 19:11:52 +0100

    Add index action for Reservation class to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/reservation/IndexAction.java
@@ -0,0 +1,64 @@
+package de.tivsource.page.admin.actions.reservation;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.ejb3plugin.InjectEJB;
+import de.tivsource.page.admin.actions.EmptyAction;
+import de.tivsource.page.dao.event.EventDaoLocal;
+import de.tivsource.page.entity.event.Event;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class IndexAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+    private static final long serialVersionUID = -2200164248790963358L;
+
+    /**
+     * Statischer Logger der Klasse.
+     */
+    private static final Logger LOGGER = Logger.getLogger(IndexAction.class);
+
+    @InjectEJB(name="EventDao")
+    private EventDaoLocal eventDaoLocal;
+
+    private Event event;
+
+    private String uncheckEvent;
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(String uncheckEvent) {
+        this.uncheckEvent = uncheckEvent;
+    }
+
+	@Override
+    @Actions({
+        @Action(
+        		value = "index", 
+        		results = { @Result(name = "success", type="tiles", location = "reservation") }
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+    	this.loadPageParameter();
+    	return SUCCESS;
+    }// Ende execute()
+
+    private void loadPageParameter() {
+        if( uncheckEvent != null && uncheckEvent != "" && uncheckEvent.length() > 0) {
+            event = eventDaoLocal.findByUuid(uncheckEvent);
+        }
+    }// Ende loadPageParameter()
+	
+}// Ende class
