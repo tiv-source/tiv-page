504e911e1e6fabc08fa66fc17bcd2e9294dcb2b4
tiv-source <marc.michele@tiv-source.de>
2015-12-25 19:11:27 +0100

    Add view action for Message class to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/message/ViewAction.java
@@ -0,0 +1,64 @@
+package de.tivsource.page.admin.actions.message;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.ejb3plugin.InjectEJB;
+import de.tivsource.page.admin.actions.EmptyAction;
+import de.tivsource.page.dao.message.MessageDaoLocal;
+import de.tivsource.page.entity.message.Message;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class ViewAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+    private static final long serialVersionUID = -2502111716063075247L;
+
+    /**
+     * Statischer Logger der Klasse.
+     */
+    private static final Logger LOGGER = Logger.getLogger(ViewAction.class);
+
+    @InjectEJB(name="MessageDao")
+    private MessageDaoLocal messageDaoLocal;
+
+    private Message message;
+
+    private String uncheckMessage;
+
+    public Message getMessage() {
+        return message;
+    }
+
+	public void setMessage(String uncheckMessage) {
+        this.uncheckMessage = uncheckMessage;
+    }
+
+	@Override
+    @Actions({
+        @Action(
+        		value = "view", 
+        		results = { @Result(name = "success", type="tiles", location = "messageView") }
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+    	this.loadPageParameter();
+    	return SUCCESS;
+    }// Ende execute()
+
+	private void loadPageParameter() {
+		if( uncheckMessage != null && uncheckMessage != "" && uncheckMessage.length() > 0) {
+		    message = messageDaoLocal.findByUuid(uncheckMessage);
+		}
+	}// Ende loadPageParameter()
+
+}// Ende class
