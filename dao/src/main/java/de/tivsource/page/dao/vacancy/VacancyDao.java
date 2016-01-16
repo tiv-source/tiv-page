9137096d4bacc4ad544b535ced4d13219effda1c
tiv-source <marc.michele@tiv-source.de>
2016-01-16 01:33:22 +0100

    Add dao interface and implementation for Vacancy class to dao project.

---
+++ dao/src/main/java/de/tivsource/page/dao/vacancy/VacancyDao.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package de.tivsource.page.dao.vacancy;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.apache.log4j.Logger;
+
+import de.tivsource.page.entity.vacancy.Vacancy;
+
+/**
+ * @author Marc Michele
+ *
+ */
+@Stateless
+public class VacancyDao implements VacancyDaoLocal {
+
+    /*
+     * Statischer Logger der Klasse.
+     */
+    private static final Logger LOGGER = Logger.getLogger(VacancyDao.class);
+
+    /*
+     * EntityManager der Dao Klasse.
+     */
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    /* (non-Javadoc)
+     * @see de.tivsource.page.dao.vacancy.VacancyDaoLocal#save(de.tivsource.page.entity.vacancy.Vacancy)
+     */
+    @Override
+    public void save(Vacancy vacancy) {
+        LOGGER.info("save(Vacancy vacancy) aufgerufen");
+        entityManager.persist(vacancy);
+    }
+
+    /* (non-Javadoc)
+     * @see de.tivsource.page.dao.vacancy.VacancyDaoLocal#merge(de.tivsource.page.entity.vacancy.Vacancy)
+     */
+    @Override
+    public void merge(Vacancy vacancy) {
+        LOGGER.info("merge(Vacancy vacancy) aufgerufen");
+        entityManager.merge(vacancy);
+    }
+
+    /* (non-Javadoc)
+     * @see de.tivsource.page.dao.vacancy.VacancyDaoLocal#delete(de.tivsource.page.entity.vacancy.Vacancy)
+     */
+    @Override
+    public void delete(Vacancy vacancy) {
+        entityManager.remove(entityManager.find(Vacancy.class, vacancy.getUuid()));
+    }
+
+    /* (non-Javadoc)
+     * @see de.tivsource.page.dao.vacancy.VacancyDaoLocal#findByUuid(java.lang.String)
+     */
+    @Override
+    public Vacancy findByUuid(String uuid) {
+        return entityManager.find(Vacancy.class, uuid);
+    }
+
+    /* (non-Javadoc)
+     * @see de.tivsource.page.dao.vacancy.VacancyDaoLocal#findAll(java.lang.Integer, java.lang.Integer)
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Vacancy> findAll(Integer start, Integer max) {
+        Query query = entityManager.createQuery("from Vacancy v");
+        query.setFirstResult(start);
+        query.setMaxResults(max);
+        return query.getResultList();
+    }
+
+    /* (non-Javadoc)
+     * @see de.tivsource.page.dao.vacancy.VacancyDaoLocal#findAll(java.lang.Integer, java.lang.Integer, java.lang.String, java.lang.String)
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Vacancy> findAll(Integer start, Integer max, String field, String order) {
+        String queryString = "SELECT DISTINCT v FROM Vacancy v JOIN v.descriptionMap dm WHERE dm.language = 'DE' ORDER BY ";
+        queryString = queryString + field + " " + order;
+        Query query = entityManager.createQuery(queryString);
+        query.setFirstResult(start);
+        query.setMaxResults(max);
+        return query.getResultList();
+    }
+
+    /* (non-Javadoc)
+     * @see de.tivsource.page.dao.vacancy.VacancyDaoLocal#countAll()
+     */
+    @Override
+    public Integer countAll() {
+        Query query = entityManager.createQuery("Select Count(v) from Vacancy v");
+        return Integer.parseInt(query.getSingleResult().toString());
+    }
+
+}// Ende class
---
+++ dao/src/main/java/de/tivsource/page/dao/vacancy/VacancyDaoLocal.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package de.tivsource.page.dao.vacancy;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import de.tivsource.page.entity.vacancy.Vacancy;
+
+/**
+ * @author Marc Michele
+ *
+ */
+@Local
+public interface VacancyDaoLocal {
+
+    /**
+     * Methode zum speichern eines Objektes der Klasse Vacancy.
+     * @param vacancy - Vacancy Objekt das gespeichert werden soll
+     */
+    public void save(Vacancy vacancy);
+
+    /**
+     * Methode zum verändern eines Objektes der Klasse Vacancy.
+     * @param vacancy - Vacancy Objekt das verändert werden soll
+     */
+    public void merge(Vacancy vacancy);
+
+    /**
+     * Methode zum löschen eines Objektes der Klasse Vacancy.
+     * @param vacancy - zu löschendes Vacancy Objekt
+     */
+    public void delete(Vacancy vacancy);
+
+    /**
+     * Methode zum laden eines Objektes der Klasse Vacancy, anhand der UUID des
+     * Objektes.
+     * 
+     * @param uuid - UUID des Objektes das geladen werden soll
+     * @return Vacany - Objekt das die angegebene UUID besitzt
+     */
+    public Vacancy findByUuid(String uuid);
+
+    /**
+     * Methode zum laden einer Liste von Vacancy Objekten, es muss dabei ein
+     * Startwert angegeben werden und die Anzahl der zu ladenen Vacancy Objekte.
+     *
+     * @param start - Startwert ab der die Liste beginnen soll
+     * @param max - Maximale Anzahl an Objekten die die Liste enthalten soll
+     * @return List<Vacancy> - Liste von Vacancy Objekten
+     */
+    public List<Vacancy> findAll(Integer start, Integer max);
+
+    /**
+     * Methode zu laden einer Liste von Vacancy Objekten, es muss dabei ein
+     * Startwert, die Anzahl der zu ladenen Objekte, das Feld nach dem sortiert
+     * werden soll und die sortier Richtung angegeben werden.
+     * 
+     * @param start
+     * @param max
+     * @param field
+     * @param order
+     * @return
+     */
+    public List<Vacancy> findAll(Integer start, Integer max, String field, String order);
+
+    /**
+     * Methode die die Anzahl aller Vacancy Objekte die sich in der Datenbank
+     * befinden zurück liefert.
+     *
+     * @return Integer - Anzahl der Vacancy Objekte die sich in der Datenbank befinden.
+     */
+    public Integer countAll();
+
+}// Ende interface
