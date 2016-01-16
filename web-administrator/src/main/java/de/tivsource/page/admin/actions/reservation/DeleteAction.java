93c295d2be8abb49eb92fb95b224ec556c774f62
tiv-source <marc.michele@tiv-source.de>
2016-01-16 21:16:15 +0100

    Add delete action for Reservation class to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/reservation/DeleteAction.java
@@ -0,0 +1,76 @@
+package de.tivsource.page.admin.actions.reservation;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.ejb3plugin.InjectEJB;
+import de.tivsource.page.admin.actions.EmptyAction;
+import de.tivsource.page.dao.reservation.ReservationDaoLocal;
+import de.tivsource.page.entity.reservation.Reservation;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class DeleteAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+    private static final long serialVersionUID = 2932939605984995920L;
+
+    /**
+     * Statischer Logger der Klasse.
+     */
+    private static final Logger LOGGER = Logger.getLogger(DeleteAction.class);
+
+    @InjectEJB(name="ReservationDao")
+    private ReservationDaoLocal reservationDaoLocal;
+
+    private String redirect;
+    
+    private Reservation reservation;
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+
+    public String getRedirect() {
+        return redirect;
+    }
+
+    @Override
+    @Actions({
+        @Action(
+        		value = "delete", 
+        		results = { 
+        				@Result(name = "success", type = "redirectAction", location = "${redirect}"),
+        				@Result(name = "input", type="tiles", location = "reservationDeleteForm"),
+        				@Result(name = "error", type="tiles", location = "reservationDeleteError")
+        				}
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+
+    	if(reservation != null) {
+    	    Reservation dbReservation = reservationDaoLocal.findByUuid(reservation.getUuid());
+    	    redirect = "index.html?event=" + dbReservation.getEvent().getUuid();
+    	    reservationDaoLocal.delete(dbReservation);
+            return SUCCESS;
+    	}
+    	else {
+    		return ERROR;
+    	}
+    	
+    	
+    }// Ende execute()
+
+}// Ende class
