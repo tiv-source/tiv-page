57ade7516881e3dbb4eda5321e2d0705342a1a47
tiv-source <marc.michele@tiv-source.de>
2016-01-16 21:17:27 +0100

    Add create action for Vacancy class to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/vacancy/AddAction.java
@@ -0,0 +1,131 @@
+package de.tivsource.page.admin.actions.vacancy;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.ejb3plugin.InjectEJB;
+import de.tivsource.page.admin.actions.EmptyAction;
+import de.tivsource.page.dao.location.LocationDaoLocal;
+import de.tivsource.page.dao.vacancy.VacancyDaoLocal;
+import de.tivsource.page.entity.enumeration.Language;
+import de.tivsource.page.entity.location.Location;
+import de.tivsource.page.entity.vacancy.Vacancy;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class AddAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+    private static final long serialVersionUID = 9114559679951589800L;
+
+    /**
+     * Statischer Logger der Klasse.
+     */
+    private static final Logger LOGGER = Logger.getLogger(AddAction.class);
+
+    @InjectEJB(name="VacancyDao")
+    private VacancyDaoLocal vacancyDaoLocal;
+
+    @InjectEJB(name="LocationDao")
+    private LocationDaoLocal locationDaoLocal;
+
+    private Vacancy vacancy;
+
+    private String lang;
+
+	public Vacancy getVacancy() {
+        return vacancy;
+    }
+
+    public void setVacancy(Vacancy vacancy) {
+        this.vacancy = vacancy;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    @Override
+    @Actions({
+        @Action(
+        		value = "add", 
+        		results = { 
+        				@Result(name = "success", type = "redirectAction", location = "index.html"),
+        				@Result(name = "input", type="tiles", location = "vacancyAddForm"),
+        				@Result(name = "error", type="tiles", location = "vacancyAddError")
+        				}
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+
+        String remoteUser    = ServletActionContext.getRequest().getRemoteUser();
+        String remoteAddress = ServletActionContext.getRequest().getRemoteAddr();
+
+    	if(vacancy != null) {
+    	    vacancy.setUuid(UUID.randomUUID().toString());
+    	    vacancy.setModified(new Date());
+    	    vacancy.setCreated(new Date());
+    	    vacancy.setModifiedBy(remoteUser);
+    	    vacancy.setIp(remoteAddress);
+
+
+    	    vacancy.getDescriptionMap().get(Language.DE).setUuid(UUID.randomUUID().toString());
+    	    vacancy.getDescriptionMap().get(Language.DE).setNamingItem(vacancy);
+    	    vacancy.getDescriptionMap().get(Language.DE).setLanguage(Language.DE);
+
+    	    vacancy.getContentMap().get(Language.DE).setUuid(UUID.randomUUID().toString());
+    	    vacancy.getContentMap().get(Language.DE).setContentItem(vacancy);
+    	    vacancy.getContentMap().get(Language.DE).setLanguage(Language.DE);
+    	    vacancy.getContentMap().get(Language.DE).setCreated(new Date());
+    	    vacancy.getContentMap().get(Language.DE).setModified(new Date());
+
+
+    	    vacancy.getDescriptionMap().get(Language.EN).setUuid(UUID.randomUUID().toString());
+    	    vacancy.getDescriptionMap().get(Language.EN).setNamingItem(vacancy);
+    	    vacancy.getDescriptionMap().get(Language.EN).setLanguage(Language.EN);
+    	    vacancy.getDescriptionMap().get(Language.EN).setDescription(vacancy.getDescriptionMap().get(Language.DE).getDescription());
+    	    vacancy.getDescriptionMap().get(Language.EN).setName(vacancy.getDescriptionMap().get(Language.DE).getName());
+    	    vacancy.getDescriptionMap().get(Language.EN).setKeywords(vacancy.getDescriptionMap().get(Language.DE).getKeywords());
+
+    	    vacancy.getContentMap().get(Language.EN).setUuid(UUID.randomUUID().toString());
+    	    vacancy.getContentMap().get(Language.EN).setContentItem(vacancy);
+    	    vacancy.getContentMap().get(Language.EN).setLanguage(Language.EN);
+    	    vacancy.getContentMap().get(Language.EN).setCreated(new Date());
+    	    vacancy.getContentMap().get(Language.EN).setModified(new Date());
+    	    vacancy.getContentMap().get(Language.EN).setContent(vacancy.getContentMap().get(Language.DE).getContent());
+    	    
+    	    
+    	    vacancyDaoLocal.merge(vacancy);
+
+            return SUCCESS;
+    	}
+    	else {
+    		return ERROR;
+    	}
+    	
+    	
+    }// Ende execute()
+
+    public List<Location> getLocationList() {
+        // TODO: Anzahl der Objkte anpassen
+        return locationDaoLocal.findAll(0, locationDaoLocal.countAll());
+    }// Ende getLocationList()
+
+}// Ende class
