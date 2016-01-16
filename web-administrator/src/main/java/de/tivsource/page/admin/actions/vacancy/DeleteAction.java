78264534d3803bc8b836810b70157fae579bb02a
tiv-source <marc.michele@tiv-source.de>
2016-01-16 21:17:08 +0100

    Add delete action for Vacancy class to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/vacancy/DeleteAction.java
@@ -0,0 +1,69 @@
+package de.tivsource.page.admin.actions.vacancy;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.ejb3plugin.InjectEJB;
+import de.tivsource.page.admin.actions.EmptyAction;
+import de.tivsource.page.dao.vacancy.VacancyDaoLocal;
+import de.tivsource.page.entity.vacancy.Vacancy;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class DeleteAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+    private static final long serialVersionUID = 3911634387524501176L;
+
+    /**
+     * Statischer Logger der Klasse.
+     */
+    private static final Logger LOGGER = Logger.getLogger(DeleteAction.class);
+
+    @InjectEJB(name="VacancyDao")
+    private VacancyDaoLocal vacancyDaoLocal;
+
+    private Vacancy vacancy;
+
+    public Vacancy getVacancy() {
+        return vacancy;
+    }
+
+    public void setVacancy(Vacancy vacancy) {
+        this.vacancy = vacancy;
+    }
+
+    @Override
+    @Actions({
+        @Action(
+        		value = "delete", 
+        		results = { 
+        				@Result(name = "success", type = "redirectAction", location = "index.html"),
+        				@Result(name = "input", type="tiles", location = "vacancyDeleteForm"),
+        				@Result(name = "error", type="tiles", location = "vacancyDeleteError")
+        				}
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+
+    	if(vacancy != null) {
+    	    Vacancy dbVacancy = vacancyDaoLocal.findByUuid(vacancy.getUuid());
+    	    vacancyDaoLocal.delete(dbVacancy);
+            return SUCCESS;
+    	}
+    	else {
+    		return ERROR;
+    	}
+    	
+    	
+    }// Ende execute()
+	
+}// Ende class
