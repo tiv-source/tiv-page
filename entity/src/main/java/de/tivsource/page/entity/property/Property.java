6bb5dc73889501b05148f40e0f65006d2784b51a
tiv-source <marc.michele@tiv-source.de>
2016-01-22 00:13:50 +0100

    Add attributes to Property class.

--- entity/src/main/java/de/tivsource/page/entity/property/Property.java
+++ entity/src/main/java/de/tivsource/page/entity/property/Property.java
@@ -3,9 +3,12 @@
  */
 package de.tivsource.page.entity.property;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
 
 import org.hibernate.search.annotations.DocumentId;
 import org.hibernate.search.annotations.Indexed;
@@ -26,6 +29,13 @@ public class Property {
     @Column(name="property_value")
     private String value;
 
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date modified;
+
+    private String modifiedBy;
+
+    private String modifiedAddress;
+
     public String getKey() {
         return key;
     }
@@ -40,6 +50,30 @@ public class Property {
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    public Date getModified() {
+        return modified;
+    }
+
+    public void setModified(Date modified) {
+        this.modified = modified;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public String getModifiedAddress() {
+        return modifiedAddress;
+    }
+
+    public void setModifiedAddress(String modifiedAddress) {
+        this.modifiedAddress = modifiedAddress;
     }
 
 }// Ende class
