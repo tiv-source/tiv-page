88588307590f47ace6839e93e5747ec9cc320d86
tiv-source <marc.michele@tiv-source.de>
2016-01-22 01:55:36 +0100

    Add new attributes to Vacancy class.

--- entity/src/main/java/de/tivsource/page/entity/vacancy/Vacancy.java
+++ entity/src/main/java/de/tivsource/page/entity/vacancy/Vacancy.java
@@ -3,6 +3,7 @@
  */
 package de.tivsource.page.entity.vacancy;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -11,6 +12,7 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
 
 import de.tivsource.page.entity.application.Application;
 import de.tivsource.page.entity.contentitem.ContentItem;
@@ -23,12 +25,43 @@ import de.tivsource.page.entity.location.Location;
 @Entity
 public class Vacancy extends ContentItem {
 
+    private String technical;
+
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date beginning;
+
+    private String workingTime;
+
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "location_uuid")
     private Location location;
 
     @OneToMany(mappedBy = "vacancy", cascade = { CascadeType.ALL }, fetch = FetchType.LAZY, orphanRemoval=true)
     private List<Application> applications;
+
+    public String getTechnical() {
+        return technical;
+    }
+
+    public void setTechnical(String technical) {
+        this.technical = technical;
+    }
+
+    public Date getBeginning() {
+        return beginning;
+    }
+
+    public void setBeginning(Date beginning) {
+        this.beginning = beginning;
+    }
+
+    public String getWorkingTime() {
+        return workingTime;
+    }
+
+    public void setWorkingTime(String workingTime) {
+        this.workingTime = workingTime;
+    }
 
     public Location getLocation() {
         return location;
