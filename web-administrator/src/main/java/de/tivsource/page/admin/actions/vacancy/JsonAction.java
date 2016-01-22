31708dd9338712a484202e1eac45620cca2f5a14
tiv-source <marc.michele@tiv-source.de>
2016-01-22 03:01:33 +0100

    Update json action of Vacancy class, extend code for order objects.

--- web-administrator/src/main/java/de/tivsource/page/admin/actions/vacancy/JsonAction.java
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/vacancy/JsonAction.java
@@ -86,75 +86,47 @@ public class JsonAction extends ActionSupport {
 		if (getSord() != null && getSord().equalsIgnoreCase("asc")) {
 		    LOGGER.info("Sortieren nach asc");
 			if (getSidx() != null && getSidx().equalsIgnoreCase("uuid")) {
-				vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.uuid", "asc");
+				vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.uuid", "asc");
 			} else if (getSidx() != null && getSidx().equalsIgnoreCase("name")) {
                 vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "dm.name", "asc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("technical")) {
-				vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.technical", "asc");
-			} else if (getSidx() != null && getSidx().equalsIgnoreCase("topNavigation")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.topNavigation", "asc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("topNavigationOrder")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.topNavigationOrder", "asc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("navigation")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.navigation", "asc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("navigationOrder")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.navigationOrder", "asc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("bottomNavigation")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.bottomNavigation", "asc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("bottomNavigationOrder")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.bottomNavigationOrder", "asc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("responsiveNavigation")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.responsiveNavigation", "asc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("responsiveNavigationOrder")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.responsiveNavigationOrder", "asc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("special")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.special", "asc");
             } else if (getSidx() != null && getSidx().equalsIgnoreCase("visible")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.visible", "asc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("modified")) {
-				vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.modified", "asc");
-			} else if (getSidx() != null && getSidx().equalsIgnoreCase("created")) {
-				vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.created", "asc");
-			} else if (getSidx() != null && getSidx().equalsIgnoreCase("visible")) {
-				vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.visible", "asc");
-			} else {
+                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.visible", "asc");
+            } else if (getSidx() != null && getSidx().equalsIgnoreCase("created")) {
+				vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.created", "asc");
+			} else if (getSidx() != null && getSidx().equalsIgnoreCase("modified")) {
+                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.modified", "asc");
+            } else if (getSidx() != null && getSidx().equalsIgnoreCase("modifiedBy")) {
+				vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.modifiedBy", "asc");
+			} else if (getSidx() != null && getSidx().equalsIgnoreCase("technical")) {
+                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.technical", "asc");
+            } else if (getSidx() != null && getSidx().equalsIgnoreCase("beginning")) {
+                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.beginning", "asc");
+            } else if (getSidx() != null && getSidx().equalsIgnoreCase("workingTime")) {
+                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.workingTime", "asc");
+            } else {
 				vacancyList = this.vacancyDaoLocal.findAll(from, getRows());
 			}
 		} else if (getSord() != null && getSord().equalsIgnoreCase("desc")) {
 		    LOGGER.info("Sortieren nach desc");
 			if (getSidx() != null && getSidx().equalsIgnoreCase("uuid")) {
-				vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.uuid", "desc");
+				vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.uuid", "desc");
 			} else if (getSidx() != null && getSidx().equalsIgnoreCase("name")) {
                 vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "dm.name", "desc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("technical")) {
-				vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.technical", "desc");
-			} else if (getSidx() != null && getSidx().equalsIgnoreCase("topNavigation")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.topNavigation", "desc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("topNavigationOrder")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.topNavigationOrder", "desc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("navigation")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.navigation", "desc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("navigationOrder")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.navigationOrder", "desc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("bottomNavigation")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.bottomNavigation", "desc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("bottomNavigationOrder")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.bottomNavigationOrder", "desc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("responsiveNavigation")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.responsiveNavigation", "desc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("responsiveNavigationOrder")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.responsiveNavigationOrder", "desc");
-            } else if (getSidx() != null && getSidx().equalsIgnoreCase("special")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.special", "desc");
             } else if (getSidx() != null && getSidx().equalsIgnoreCase("visible")) {
-                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.visible", "desc");
+                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.visible", "desc");
+            } else if (getSidx() != null && getSidx().equalsIgnoreCase("created")) {
+                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.created", "desc");
             } else if (getSidx() != null && getSidx().equalsIgnoreCase("modified")) {
-				vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.modified", "desc");
-			} else if (getSidx() != null && getSidx().equalsIgnoreCase("created")) {
-				vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.created", "desc");
-			} else if (getSidx() != null && getSidx().equalsIgnoreCase("visible")) {
-				vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "p.visible", "desc");
-			} else {
+                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.modified", "desc");
+            } else if (getSidx() != null && getSidx().equalsIgnoreCase("modifiedBy")) {
+                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.modifiedBy", "desc");
+            } else if (getSidx() != null && getSidx().equalsIgnoreCase("technical")) {
+                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.technical", "desc");
+            } else if (getSidx() != null && getSidx().equalsIgnoreCase("beginning")) {
+                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.beginning", "desc");
+            } else if (getSidx() != null && getSidx().equalsIgnoreCase("workingTime")) {
+                vacancyList = this.vacancyDaoLocal.findAll(from, getRows(), "v.workingTime", "desc");
+            } else {
 				vacancyList = this.vacancyDaoLocal.findAll(from, getRows());
 			}
 		}
