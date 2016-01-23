df8e168384fa12dbe6ac1412411ef1753d783ef9
tiv-source <marc.michele@tiv-source.de>
2016-01-23 20:20:50 +0100

    Add PictureDescription class to entity project.

---
+++ entity/src/main/java/de/tivsource/page/entity/picture/PictureDescription.java
@@ -0,0 +1,97 @@
+package de.tivsource.page.entity.picture;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.search.annotations.DocumentId;
+
+import de.tivsource.page.entity.enumeration.Language;
+
+/**
+ * 
+ * @author Marc Michele
+ * 
+ */
+@Entity
+public class PictureDescription {
+
+    /**
+     * UUID des Objektes der Klasse PictureDescription, diese ID ist einmalig über alle
+     * Objekte hinweg und sollte der bevorzugte weg sein auf bestimmte Objekte
+     * zuzugreifen.
+     */
+    @Id
+    @DocumentId
+    @Column(name = "uuid", unique = true)
+    private String uuid;
+
+    private String name;
+
+    @Lob
+    private String description;
+
+    private String keywords;
+
+    @Enumerated(EnumType.STRING)
+    private Language language;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "picture_uuid")
+    private Picture picture;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+	public Picture getPicture() {
+		return picture;
+	}
+
+	public void setPicture(Picture picture) {
+		this.picture = picture;
+	}
+
+}// Ende class
