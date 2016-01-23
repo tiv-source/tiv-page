e92b5d66d339409e691eb8a25ad97361a0358ffd
tiv-source <marc.michele@tiv-source.de>
2016-01-23 22:07:23 +0100

    Add index action for Gallery class to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/gallery/IndexAction.java
@@ -0,0 +1,39 @@
+package de.tivsource.page.admin.actions.gallery;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.page.admin.actions.EmptyAction;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class IndexAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+	private static final long serialVersionUID = -3178996543889256154L;
+
+	/**
+	 * Statischer Logger der Klasse.
+	 */
+    private static final Logger LOGGER = Logger.getLogger(IndexAction.class);
+
+	@Override
+    @Actions({
+        @Action(
+        		value = "index", 
+        		results = { @Result(name = "success", type="tiles", location = "gallery") }
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+    	return SUCCESS;
+    }// Ende execute()
+
+}// Ende class
