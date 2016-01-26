63f3615de1f76227233e791554fb7fe64e837035
tiv-source <marc.michele@tiv-source.de>
2016-01-26 21:52:06 +0100

    Add delete action for Gallery class to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/gallery/DeleteAction.java
@@ -0,0 +1,69 @@
+package de.tivsource.page.admin.actions.gallery;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.ejb3plugin.InjectEJB;
+import de.tivsource.page.admin.actions.EmptyAction;
+import de.tivsource.page.dao.gallery.GalleryDaoLocal;
+import de.tivsource.page.entity.gallery.Gallery;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class DeleteAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+	private static final long serialVersionUID = -4785386555841300052L;
+
+	/**
+     * Statischer Logger der Klasse.
+     */
+    private static final Logger LOGGER = Logger.getLogger(DeleteAction.class);
+
+    @InjectEJB(name="GalleryDao")
+    private GalleryDaoLocal galleryDaoLocal;
+
+    private Gallery gallery;
+
+    public Gallery getGallery() {
+        return gallery;
+    }
+
+    public void setGallery(Gallery gallery) {
+        this.gallery = gallery;
+    }
+
+    @Override
+    @Actions({
+        @Action(
+        		value = "delete", 
+        		results = { 
+        				@Result(name = "success", type = "redirectAction", location = "index.html"),
+        				@Result(name = "input", type="tiles", location = "galleryDeleteForm"),
+        				@Result(name = "error", type="tiles", location = "galleryDeleteError")
+        				}
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+
+    	if(gallery != null) {
+    		Gallery dbGallery = galleryDaoLocal.findByUuid(gallery.getUuid());
+    	    galleryDaoLocal.delete(dbGallery);
+            return SUCCESS;
+    	}
+    	else {
+    		return ERROR;
+    	}
+    	
+    	
+    }// Ende execute()
+	
+}// Ende class
