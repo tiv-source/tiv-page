8d5e1f8748814f4467989443e02fd0db33c95b9b
tiv-source <marc.michele@tiv-source.de>
2016-01-30 21:35:57 +0100

    Update edit action of Location class, add code for picture attribute.

--- web-administrator/src/main/java/de/tivsource/page/admin/actions/location/EditAction.java
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/location/EditAction.java
@@ -1,6 +1,7 @@
 package de.tivsource.page.admin.actions.location;
 
 import java.util.Date;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.ServletActionContext;
@@ -11,8 +12,10 @@ import org.apache.struts2.convention.annotation.Result;
 import de.tivsource.ejb3plugin.InjectEJB;
 import de.tivsource.page.admin.actions.EmptyAction;
 import de.tivsource.page.dao.location.LocationDaoLocal;
+import de.tivsource.page.dao.picture.PictureDaoLocal;
 import de.tivsource.page.entity.enumeration.Language;
 import de.tivsource.page.entity.location.Location;
+import de.tivsource.page.entity.picture.Picture;
 
 /**
  * 
@@ -33,6 +36,9 @@ public class EditAction extends EmptyAction {
 
     @InjectEJB(name="LocationDao")
     private LocationDaoLocal locationDaoLocal;
+
+    @InjectEJB(name="PictureDao")
+    private PictureDaoLocal pictureDaoLocal;
 
     private Location location;
 
@@ -105,6 +111,7 @@ public class EditAction extends EmptyAction {
     		dbLocation.setEvent(location.getEvent());
     		dbLocation.setLatitude(location.getLatitude());
     		dbLocation.setLongitude(location.getLongitude());
+    		dbLocation.setPicture(location.getPicture());
 
     		locationDaoLocal.merge(dbLocation);
             return SUCCESS;
@@ -115,5 +122,9 @@ public class EditAction extends EmptyAction {
 
     }// Ende execute()
 
+	public List<Picture> getPictureList() {
+		// TODO: Gallery UUID aus den Einstellungen auslesen und setzen
+		return pictureDaoLocal.findAll("d8a2d89f-cda4-4c64-9e51-18592e88bbc6");
+	}
 
 }// Ende class
