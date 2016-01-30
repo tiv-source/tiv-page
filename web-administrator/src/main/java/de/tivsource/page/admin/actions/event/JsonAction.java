a2d29adb179507675ede9315f177a3c0acc49601
tiv-source <marc.michele@tiv-source.de>
2016-01-30 21:33:49 +0100

    Update json action of Event class, add code for picture attribute.

--- web-administrator/src/main/java/de/tivsource/page/admin/actions/event/JsonAction.java
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/event/JsonAction.java
@@ -48,7 +48,7 @@ public class JsonAction extends ActionSupport {
     @Action(
             value = "table",
             results = {
-                @Result(name = "success", type="json", params={"excludeProperties", "gridModel.*.reservations, gridModel.*.events, gridModel.*.vacancies"})
+                @Result(name = "success", type="json", params={"excludeProperties", "gridModel.*.reservations, gridModel.*.events, gridModel.*.vacancies, gridModel.*.picture"})
             }
         )
 	public String execute() {
