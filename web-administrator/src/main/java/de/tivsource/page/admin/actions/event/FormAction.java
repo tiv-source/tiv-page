8e592438d89ba77b0a0f3811aa41a7f094186355
tiv-source <marc.michele@tiv-source.de>
2016-02-12 23:33:06 +0100

    Remove unnecessary annotation from form action of Event class.

--- web-administrator/src/main/java/de/tivsource/page/admin/actions/event/FormAction.java
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/event/FormAction.java
@@ -81,10 +81,6 @@ public class FormAction extends EmptyAction {
         @Action(
         		value = "deleteForm", 
         		results = { @Result(name = "success", type="tiles", location = "eventDeleteForm") }
-        ),
-        @Action(
-                value = "pictureForm", 
-                results = { @Result(name = "success", type="tiles", location = "eventPictureForm") }
         )
     })
     public String execute() throws Exception {
