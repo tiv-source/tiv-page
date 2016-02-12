0f253f800c42956efc9944a2cf0d5858db42a7c2
tiv-source <marc.michele@tiv-source.de>
2016-02-12 23:25:27 +0100

    Add new attributes to Application class.

--- entity/src/main/java/de/tivsource/page/entity/application/Application.java
+++ entity/src/main/java/de/tivsource/page/entity/application/Application.java
@@ -4,17 +4,24 @@
 package de.tivsource.page.entity.application;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 
 import org.hibernate.search.annotations.DocumentId;
 
+import de.tivsource.page.entity.embeddable.Address;
+import de.tivsource.page.entity.embeddable.ContactDetails;
 import de.tivsource.page.entity.vacancy.Vacancy;
 
 /**
@@ -34,6 +41,35 @@ public class Application {
     @Column(name="uuid", unique=true)
     private String uuid;
 
+	/**
+	 * Geschlecht des Kontaktes.
+	 */
+    @Basic
+    @org.hibernate.annotations.Type(type = "yes_no")
+    private Boolean gender;
+
+	/**
+	 * Vorname des Kontakes.
+	 */
+	private String firstname;
+
+	/**
+	 * Nachname des Kontaktes.
+	 */
+	private String lastname;
+
+    @Embedded
+    private Address address;
+
+    @Embedded
+    private ContactDetails contactDetails;
+
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date birthday;
+
+    @OneToMany(mappedBy = "application", cascade = { CascadeType.ALL }, fetch = FetchType.EAGER, orphanRemoval=true)
+    private List<Education> educations;
+
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "vacancy_uuid")
     private Vacancy vacancy;
@@ -41,6 +77,6 @@ public class Application {
     @Temporal(javax.persistence.TemporalType.TIMESTAMP)
     private Date created;
 
-    private String ip;
+    private String createdAddress;
 
 }// Ende class
