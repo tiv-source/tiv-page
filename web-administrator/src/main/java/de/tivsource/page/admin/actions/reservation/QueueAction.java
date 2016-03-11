568fea539e3cee38efba79f699e6f9406cdaedd4
tiv-source <marc.michele@tiv-source.de>
2016-03-11 19:38:14 +0100

    Add queue action for Reservation class to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/reservation/QueueAction.java
@@ -0,0 +1,39 @@
+package de.tivsource.page.admin.actions.reservation;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.page.admin.actions.EmptyAction;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class QueueAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+	private static final long serialVersionUID = 4599503878936885414L;
+
+	/**
+	 * Statischer Logger der Klasse.
+	 */
+    private static final Logger LOGGER = Logger.getLogger(QueueAction.class);
+
+	@Override
+    @Actions({
+        @Action(
+        		value = "queue", 
+        		results = { @Result(name = "success", type="tiles", location = "reservationQueue") }
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+    	return SUCCESS;
+    }// Ende execute()
+
+}// Ende class
