a433d781937787efc6e9f6e19197b76fdd8e2327
tiv-source <marc.michele@tiv-source.de>
2016-03-19 23:29:02 +0100

    Update remove attributes from News class.

--- entity/src/main/java/de/tivsource/page/entity/news/News.java
+++ entity/src/main/java/de/tivsource/page/entity/news/News.java
@@ -9,7 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.Temporal;
 
 import de.tivsource.page.entity.contentitem.ContentItem;
-import de.tivsource.page.entity.enumeration.IconType;
 
 /**
  * @author Marc Michele
@@ -18,22 +17,8 @@ import de.tivsource.page.entity.enumeration.IconType;
 @Entity
 public class News extends ContentItem {
 
-	private IconType icon;
-
 	@Temporal(javax.persistence.TemporalType.TIMESTAMP)
 	private Date releaseDate;
-
-	public News() {
-		super();
-	}
-
-	public IconType getIcon() {
-		return icon;
-	}
-
-	public void setIcon(IconType icon) {
-		this.icon = icon;
-	}
 
 	public Date getReleaseDate() {
 		return releaseDate;
