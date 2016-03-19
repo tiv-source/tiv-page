021dc7171d3839edc7d8fbd1a09a33982f6433b0
tiv-source <marc.michele@tiv-source.de>
2016-03-20 00:42:01 +0100

    Add first version of confrim action to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/reservation/ConfirmAction.java
@@ -0,0 +1,123 @@
+package de.tivsource.page.admin.actions.reservation;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.ejb3plugin.InjectEJB;
+import de.tivsource.page.admin.actions.EmptyAction;
+import de.tivsource.page.dao.event.EventDaoLocal;
+import de.tivsource.page.dao.reservation.ReservationDaoLocal;
+import de.tivsource.page.entity.reservation.Reservation;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class ConfirmAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+    private static final long serialVersionUID = 7464881886808879895L;
+
+    /**
+     * Statischer Logger der Klasse.
+     */
+    private static final Logger LOGGER = Logger.getLogger(ConfirmAction.class);
+
+    @InjectEJB(name="EventDao")
+    private EventDaoLocal eventDaoLocal;
+
+    @InjectEJB(name="ReservationDao")
+    private ReservationDaoLocal reservationDaoLocal;
+
+    private String redirect;
+    
+    private Reservation reservation;
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+
+    public String getRedirect() {
+        return redirect;
+    }
+
+    @Override
+    @Actions({
+        @Action(
+        		value = "confirm", 
+        		results = { 
+        				@Result(name = "success", type = "redirectAction", location = "queue.html"),
+        				@Result(name = "input", type = "tiles", location = "reservationConfirmForm"),
+        				@Result(name = "error", type = "tiles", location = "reservationConfirmError")
+        				}
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+
+        String remoteUser    = ServletActionContext.getRequest().getRemoteUser();
+        String remoteAddress = ServletActionContext.getRequest().getRemoteAddr();
+
+    	if(reservation != null) {
+    	    Reservation dbReservation = reservationDaoLocal.findByUuid(reservation.getUuid());
+    	    dbReservation.setConfirmed(true);
+    	    dbReservation.setConfirmedAddress(remoteAddress);
+    	    dbReservation.setConfirmedDate(new Date());
+    	    dbReservation.setConfirmedBy(remoteUser);
+    	    reservationDaoLocal.merge(dbReservation);
+            return SUCCESS;
+    	}
+    	else {
+    		return ERROR;
+    	}
+    	
+    	
+    }// Ende execute()
+
+    public List<Date> getTimes() {
+        LOGGER.info("getTimes() aufgerufen.");
+
+        List<Date> times = new ArrayList<Date>();
+
+        // Anfangs Punkt der Zeitreihe
+        Calendar calendarStart = Calendar.getInstance();
+        calendarStart.setTime(reservation.getEvent().getBeginning());
+        times.add(calendarStart.getTime());
+                
+        // Endpunkt der Zeitreihe 
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(reservation.getEvent().getEnding());
+        calendar.add(Calendar.MINUTE, -30);
+        Date end = calendar.getTime();
+
+        // Datum mit dem gerechnet wird
+        Date time = reservation.getEvent().getBeginning();
+        while (time.before(end)) {
+            Calendar calendarTime = Calendar.getInstance();
+            calendarTime.setTime(time);
+            calendarTime.add(Calendar.MINUTE, 15);
+            time = calendarTime.getTime();
+            times.add(time);
+        }
+
+        LOGGER.info("Inhalt der Liste: " + times.size());
+
+        return times;
+    }
+
+}// Ende class
