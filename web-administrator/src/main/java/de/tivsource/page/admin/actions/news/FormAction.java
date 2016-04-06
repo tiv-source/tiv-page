db37993d1ab52c3945c7f22d4d6e4897de208a52
tiv-source <marc.michele@tiv-source.de>
2016-04-07 01:42:48 +0200

    Add form action for News class to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/news/FormAction.java
@@ -0,0 +1,99 @@
+package de.tivsource.page.admin.actions.news;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.ejb3plugin.InjectEJB;
+import de.tivsource.page.admin.actions.EmptyAction;
+import de.tivsource.page.dao.news.NewsDaoLocal;
+import de.tivsource.page.dao.picture.PictureDaoLocal;
+import de.tivsource.page.entity.news.News;
+import de.tivsource.page.entity.picture.Picture;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class FormAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+	private static final long serialVersionUID = -2195892317449706457L;
+
+	/**
+     * Statischer Logger der Klasse.
+     */
+    private static final Logger LOGGER = Logger.getLogger(FormAction.class);
+
+	@InjectEJB(name="NewsDao")
+    private NewsDaoLocal newsDaoLocal;
+
+    @InjectEJB(name="PictureDao")
+    private PictureDaoLocal pictureDaoLocal;
+
+	private News news;
+
+	private String uncheckNews;
+
+	private String lang;
+
+	public News getNews() {
+        return news;
+    }
+
+	public void setNews(String uncheckNews) {
+        this.uncheckNews = uncheckNews;
+    }
+
+	public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    @Override
+    @Actions({
+        @Action(
+        		value = "editForm", 
+        		results = { @Result(name = "success", type="tiles", location = "newsEditForm") }
+        ),
+        @Action(
+        		value = "addForm", 
+        		results = { @Result(name = "success", type="tiles", location = "newsAddForm") }
+        ),
+        @Action(
+        		value = "deleteForm", 
+        		results = { @Result(name = "success", type="tiles", location = "newsDeleteForm") }
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+    	
+    	this.loadPageParameter();
+    	return SUCCESS;
+    }// Ende execute()
+
+	private void loadPageParameter() {
+
+		if( uncheckNews != null && uncheckNews != "" && uncheckNews.length() > 0) {
+			news = newsDaoLocal.findByUuid(uncheckNews);
+		} else {
+			news = new News();
+		}
+
+	}// Ende loadPageParameter()
+
+	public List<Picture> getPictureList() {
+		// TODO: Gallery UUID aus den Einstellungen auslesen und setzen
+		return pictureDaoLocal.findAll("beb3351d-9303-43d3-8c91-62e892199227");
+	}
+
+}// Ende class
