53c70ad832a986bf55f15b9bb19ec09b89a8429f
tiv-source <marc.michele@tiv-source.de>
2016-04-07 00:22:16 +0200

    Add action for osm cache creation to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/location/OSMCacheAction.java
@@ -0,0 +1,59 @@
+package de.tivsource.page.admin.actions.location;
+
+
+import java.util.Iterator;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+
+import de.tivsource.ejb3plugin.InjectEJB;
+import de.tivsource.page.admin.actions.EmptyAction;
+import de.tivsource.page.dao.location.LocationDaoLocal;
+import de.tivsource.page.entity.location.Location;
+import de.tivsource.page.helper.osm.CreateOpenStreetMapCache;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+public class OSMCacheAction extends EmptyAction {
+
+	/**
+	 * Serial Version UID.
+	 */
+	private static final long serialVersionUID = 575079215033958866L;
+
+	/**
+	 * Statischer Logger der Klasse.
+	 */
+    private static final Logger LOGGER = Logger.getLogger(OSMCacheAction.class);
+
+    @InjectEJB(name="LocationDao")
+    private LocationDaoLocal locationDaoLocal;
+
+    @Override
+    @Actions({
+        @Action(
+        		value = "cache", 
+        		results = { @Result(name = "success", type="tiles", location = "index") }
+        )
+    })
+    public String execute() throws Exception {
+    	LOGGER.info("execute() aufgerufen.");
+    	
+    	Iterator<Location> locationIterator = locationDaoLocal.findAll(0, locationDaoLocal.countAll()).iterator();
+    	
+    	while(locationIterator.hasNext()) {
+    		Location next = locationIterator.next();
+    		CreateOpenStreetMapCache cacheToCreate = new CreateOpenStreetMapCache(next.getUuid(), next.getLatitude(), next.getLongitude());
+    		cacheToCreate.generate();
+    	}
+    	
+    	
+    	return SUCCESS;
+    }// Ende execute()
+	
+}// Ende class
