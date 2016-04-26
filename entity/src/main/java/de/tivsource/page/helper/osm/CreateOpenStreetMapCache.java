25d9811721bc0b5a69bf7b3fffcb7aa4a242a2c1
tiv-source <marc.michele@tiv-source.de>
2016-04-26 23:37:08 +0200

    Update code for osm cache creation, generate several widths.

--- entity/src/main/java/de/tivsource/page/helper/osm/CreateOpenStreetMapCache.java
+++ entity/src/main/java/de/tivsource/page/helper/osm/CreateOpenStreetMapCache.java
@@ -10,6 +10,9 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
@@ -30,61 +33,100 @@ public class CreateOpenStreetMapCache {
 	// <struts:property value="latitude" />,<struts:property value="longitude" />
 	// <struts:property value="latitude" />,<struts:property value="longitude" />
 	private String url = "http://staticmap.openstreetmap.de/staticmap.php?center=";
-	private String urlParameter1 = "&zoom=18&size=250x230&maptype=mapnik&markers=";
-	private String urlParameter2 = ",lightblue";
+	private String urlParameter1 = "&zoom=18&size=";
+	private String urlParameter2 = "&maptype=mapnik&markers=";
+	private String urlParameter3 = ",lightblue";
+
+	private Map<String, String> widthMap;
 
 	public CreateOpenStreetMapCache(String uuid, String latitude,
 			String longitude) {
 		super();
+		widthMap = new HashMap<String, String>();
+		initWidth();
 		this.uuid = uuid;
 		this.centerLatitude = latitude;
 		this.centerLongitude = longitude;
 	}
 
     public void generate() {
-    	StringBuffer urlStringBuffer = new StringBuffer();
-    	urlStringBuffer.append(url);
-    	urlStringBuffer.append(centerLatitude);
-    	urlStringBuffer.append(",");
-    	urlStringBuffer.append(centerLongitude);
-    	urlStringBuffer.append(urlParameter1);
-    	urlStringBuffer.append(centerLatitude);
-    	urlStringBuffer.append(",");
-    	urlStringBuffer.append(centerLongitude);
-    	urlStringBuffer.append(urlParameter2);
+    	Iterator<String> keys = widthMap.keySet().iterator();
+		while(keys.hasNext()) {
+			String key = keys.next();
+			loadImages(
+					constructUrl(widthMap.get(key)), 
+					constructPath(key)
+					);
+		}
+	}// Ende generate()
 
-    	StringBuffer pictureStringBuffer = new StringBuffer();
-    	pictureStringBuffer.append(HTDOCS);
-    	pictureStringBuffer.append(uuid);
-    	pictureStringBuffer.append(".png");
-    	
+    private void initWidth() {
+    	widthMap.put("wdefault", "250x230");
+    	widthMap.put("w0201",    "250x250");
+    	widthMap.put("w0581",    "230x465");
+    	widthMap.put("w0619",    "232x450");
+    	widthMap.put("w0641",    "270x467");
+    	widthMap.put("w0701",    "300x450");
+    	widthMap.put("w0901",    "300x465");
+    	widthMap.put("w0951",    "310x450");
+    	widthMap.put("w1001",    "340x452");
+    	widthMap.put("w1101",    "440x455");
+    	widthMap.put("w1291",    "490x455");
+    	widthMap.put("w1321",    "540x455");
+    	widthMap.put("w1401",    "590x455");
+    	widthMap.put("w1501",    "640x458");
+    	widthMap.put("w1701",    "740x458");
+    }
+
+    private void loadImages(String urlString, String filePath) {
         BufferedImage image =null;
         try {
-        	URL url =new URL(urlStringBuffer.toString());
-
-            // read the url
+        	URL url = new URL(urlString);
+            // Lade das Bild
             image = ImageIO.read(url);
-            String pictureString = pictureStringBuffer.toString();
-            Path picturePath = Paths.get(pictureString);
 
-            
-            
+            Path picturePath = Paths.get(filePath);
+
 			if (Files.exists(picturePath) && !Files.isDirectory(picturePath)
 					&& Files.isRegularFile(picturePath)) {
 				// Lösche die Datei
             	Files.delete(picturePath);
             	// Speichere die Datei
-            	ImageIO.write(image, "png",new File(pictureString));
+            	ImageIO.write(image, "png",new File(filePath));
             } else if(Files.notExists(picturePath)) {
             	// Speichere die Datei
-            	ImageIO.write(image, "png",new File(pictureString));
+            	ImageIO.write(image, "png",new File(filePath));
             }
 
         } catch(IOException e) {
 	            e.printStackTrace();
 	    }
+    }
+    
+    private String constructUrl(String size) {
+    	StringBuffer stringBuffer = new StringBuffer();
+    	stringBuffer.append(url);
+    	stringBuffer.append(centerLatitude);
+    	stringBuffer.append(",");
+    	stringBuffer.append(centerLongitude);
+    	stringBuffer.append(urlParameter1);
+    	stringBuffer.append(size);
+    	stringBuffer.append(urlParameter2);
+    	stringBuffer.append(centerLatitude);
+    	stringBuffer.append(",");
+    	stringBuffer.append(centerLongitude);
+    	stringBuffer.append(urlParameter3);
+    	return stringBuffer.toString();
+    }
 
-		
-	}// Ende generate()
+    private String constructPath(String width) {
+    	StringBuffer stringBuffer = new StringBuffer();
+    	stringBuffer.append(HTDOCS);
+    	stringBuffer.append(uuid);
+    	stringBuffer.append("_");
+    	stringBuffer.append(width);
+    	stringBuffer.append(".png");
+    	return stringBuffer.toString();
+    }
 
 }// Ende class
