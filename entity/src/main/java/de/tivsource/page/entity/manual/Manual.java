123149a68bfe4d32ba68700e65e5cde0e240475e
tiv-source <marc.michele@tiv-source.de>
2016-06-03 00:37:54 +0200

    Add Manual class to entity project.

---
+++ entity/src/main/java/de/tivsource/page/entity/manual/Manual.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package de.tivsource.page.entity.manual;
+
+import javax.persistence.Entity;
+
+/**
+ * @author Marc Michele
+ *
+ */
+@Entity
+public class Manual {
+
+}// Ende class
