9810977c4004f1e4c160f3db90d75522579b5fcd
tiv-source <marc.michele@tiv-source.de>
2016-06-03 02:00:59 +0200

    Add class Appointment to entity project.

---
+++ entity/src/main/java/de/tivsource/page/entity/appointment/Appointment.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package de.tivsource.page.entity.appointment;
+
+import java.util.Date;
+
+/**
+ * @author Marc Michele
+ *
+ */
+public class Appointment {
+	
+	private Date pointInTime;
+
+}// Ende class
