14adf9aa81f15ed0aa41a78ff79dbe63ecbc3623
tiv-source <marc.michele@tiv-source.de>
2016-06-03 02:02:23 +0200

    Extends class Manual.

--- entity/src/main/java/de/tivsource/page/entity/manual/Manual.java
+++ entity/src/main/java/de/tivsource/page/entity/manual/Manual.java
@@ -5,11 +5,13 @@ package de.tivsource.page.entity.manual;
 
 import javax.persistence.Entity;
 
+import de.tivsource.page.entity.contentitem.ContentItem;
+
 /**
  * @author Marc Michele
  *
  */
 @Entity
-public class Manual {
+public class Manual extends ContentItem {
 
 }// Ende class
