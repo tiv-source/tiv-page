5088c8cc4e296c140d51692b0de14213a59057d4
tiv-source <marc.michele@tiv-source.de>
2016-08-07 00:26:54 +0200

    Add index action for locations namespace to web-administrator project.

---
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/locations/IndexAction.java
@@ -0,0 +1,50 @@
+package de.tivsource.page.admin.actions.locations;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.tiles.annotation.TilesDefinition;
+import org.apache.struts2.tiles.annotation.TilesDefinitions;
+import org.apache.struts2.tiles.annotation.TilesPutAttribute;
+
+import de.tivsource.page.admin.actions.EmptyAction;
+
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+@TilesDefinitions({
+    @TilesDefinition(name="locations", extend = "adminTemplate", putAttributes = {
+        @TilesPutAttribute(name = "navigation", value = "/WEB-INF/tiles/active/navigation/locations.jsp"),
+        @TilesPutAttribute(name = "content", value = "/WEB-INF/tiles/active/view/locations.jsp")
+        
+    })
+})
+public class IndexAction extends EmptyAction {
+
+    /**
+     * Serial Version UID.
+     */
+	private static final long serialVersionUID = 5222465561358934965L;
+
+	/**
+     * Statischer Logger der Klasse.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(IndexAction.class);
+
+    @Override
+    @Actions({ 
+        @Action(
+            value = "index", 
+            results = { @Result(name = "success", type = "tiles", location = "locations") }
+        ) 
+    })
+    public String execute() throws Exception {
+        LOGGER.info("execute() aufgerufen.");
+        return SUCCESS;
+    }// Ende execute()
+
+}// Ende class
