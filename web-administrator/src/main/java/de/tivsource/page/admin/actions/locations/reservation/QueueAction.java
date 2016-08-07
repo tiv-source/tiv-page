3418e9b1e1441aaf75605e96d7413c904aaafccc
tiv-source <marc.michele@tiv-source.de>
2016-08-07 22:33:45 +0200

    Add tiles definition to queue action of Reservation class.

--- web-administrator/src/main/java/de/tivsource/page/admin/actions/locations/reservation/QueueAction.java
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/locations/reservation/QueueAction.java
@@ -5,6 +5,9 @@ import org.apache.logging.log4j.Logger;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Actions;
 import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.tiles.annotation.TilesDefinition;
+import org.apache.struts2.tiles.annotation.TilesDefinitions;
+import org.apache.struts2.tiles.annotation.TilesPutAttribute;
 
 import de.tivsource.page.admin.actions.EmptyAction;
 
@@ -13,6 +16,13 @@ import de.tivsource.page.admin.actions.EmptyAction;
  * @author Marc Michele
  *
  */
+@TilesDefinitions({
+  @TilesDefinition(name="reservationQueue", extend = "adminTemplate", putAttributes = {
+    @TilesPutAttribute(name = "meta",       value = "/WEB-INF/tiles/active/meta/default_jquery.jsp"),
+    @TilesPutAttribute(name = "navigation", value = "/WEB-INF/tiles/active/navigation/locations.jsp"),
+    @TilesPutAttribute(name = "content",    value = "/WEB-INF/tiles/active/view/reservation/queue.jsp")
+  })
+})
 public class QueueAction extends EmptyAction {
 
 	/**
