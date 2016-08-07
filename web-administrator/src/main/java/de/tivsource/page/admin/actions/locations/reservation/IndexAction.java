939376bd68e6955857c6e9a4fb8227d9f924644d
tiv-source <marc.michele@tiv-source.de>
2016-08-07 22:33:22 +0200

    Add tiles definition to index action of Reservation class.

--- web-administrator/src/main/java/de/tivsource/page/admin/actions/locations/reservation/IndexAction.java
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/locations/reservation/IndexAction.java
@@ -5,6 +5,9 @@ import org.apache.logging.log4j.Logger;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Actions;
 import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.tiles.annotation.TilesDefinition;
+import org.apache.struts2.tiles.annotation.TilesDefinitions;
+import org.apache.struts2.tiles.annotation.TilesPutAttribute;
 
 import de.tivsource.ejb3plugin.InjectEJB;
 import de.tivsource.page.admin.actions.EmptyAction;
@@ -16,6 +19,13 @@ import de.tivsource.page.entity.event.Event;
  * @author Marc Michele
  *
  */
+@TilesDefinitions({
+  @TilesDefinition(name="reservationGrid", extend = "adminTemplate", putAttributes = {
+    @TilesPutAttribute(name = "meta",       value = "/WEB-INF/tiles/active/meta/default_jquery.jsp"),
+    @TilesPutAttribute(name = "navigation", value = "/WEB-INF/tiles/active/navigation/locations.jsp"),
+    @TilesPutAttribute(name = "content",    value = "/WEB-INF/tiles/active/view/reservation/list.jsp")
+  })
+})
 public class IndexAction extends EmptyAction {
 
 	/**
@@ -47,7 +57,7 @@ public class IndexAction extends EmptyAction {
     @Actions({
         @Action(
         		value = "index", 
-        		results = { @Result(name = "success", type="tiles", location = "reservation") }
+        		results = { @Result(name = "success", type="tiles", location = "reservationGrid") }
         )
     })
     public String execute() throws Exception {
