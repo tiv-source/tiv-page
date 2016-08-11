0dd108fce6482b05653bc5bedbaa1d950b0ef324
tiv-source <marc.michele@tiv-source.de>
2016-08-11 21:32:20 +0200

    Add tiles annotation to view action of Message class.

--- web-administrator/src/main/java/de/tivsource/page/admin/actions/others/message/ViewAction.java
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/others/message/ViewAction.java
@@ -5,6 +5,9 @@ import org.apache.logging.log4j.Logger;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Actions;
 import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.tiles.annotation.TilesDefinition;
+import org.apache.struts2.tiles.annotation.TilesDefinitions;
+import org.apache.struts2.tiles.annotation.TilesPutAttribute;
 
 import de.tivsource.ejb3plugin.InjectEJB;
 import de.tivsource.page.admin.actions.EmptyAction;
@@ -16,6 +19,12 @@ import de.tivsource.page.entity.message.Message;
  * @author Marc Michele
  *
  */
+@TilesDefinitions({
+  @TilesDefinition(name="messageView",  extend = "adminTemplate", putAttributes = {
+    @TilesPutAttribute(name = "navigation", value = "/WEB-INF/tiles/active/navigation/others.jsp"),
+    @TilesPutAttribute(name = "content",    value = "/WEB-INF/tiles/active/view/message/view.jsp")
+  })
+})
 public class ViewAction extends EmptyAction {
 
 	/**
