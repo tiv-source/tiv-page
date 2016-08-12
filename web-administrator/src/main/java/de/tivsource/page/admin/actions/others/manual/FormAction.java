bde7de03860edf6f3e0e792e453b2f092a52257c
tiv-source <marc.michele@tiv-source.de>
2016-08-13 00:37:50 +0200

    Add tiles annotation to form action of Manual class.

--- web-administrator/src/main/java/de/tivsource/page/admin/actions/others/manual/FormAction.java
+++ web-administrator/src/main/java/de/tivsource/page/admin/actions/others/manual/FormAction.java
@@ -7,6 +7,9 @@ import org.apache.logging.log4j.Logger;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Actions;
 import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.tiles.annotation.TilesDefinition;
+import org.apache.struts2.tiles.annotation.TilesDefinitions;
+import org.apache.struts2.tiles.annotation.TilesPutAttribute;
 
 import de.tivsource.ejb3plugin.InjectEJB;
 import de.tivsource.page.admin.actions.EmptyAction;
@@ -20,6 +23,22 @@ import de.tivsource.page.entity.picture.Picture;
  * @author Marc Michele
  *
  */
+@TilesDefinitions({
+  @TilesDefinition(name="manualAddForm",  extend = "adminTemplate", putAttributes = {
+    @TilesPutAttribute(name = "meta",       value = "/WEB-INF/tiles/active/meta/chosen.jsp"),
+    @TilesPutAttribute(name = "navigation", value = "/WEB-INF/tiles/active/navigation/others.jsp"),
+    @TilesPutAttribute(name = "content",    value = "/WEB-INF/tiles/active/view/manual/add_form.jsp")
+  }),
+  @TilesDefinition(name="manualEditForm", extend = "adminTemplate", putAttributes = {
+    @TilesPutAttribute(name = "meta",       value = "/WEB-INF/tiles/active/meta/chosen.jsp"),
+    @TilesPutAttribute(name = "navigation", value = "/WEB-INF/tiles/active/navigation/others.jsp"),
+    @TilesPutAttribute(name = "content",    value = "/WEB-INF/tiles/active/view/manual/edit_form.jsp")
+  }),
+  @TilesDefinition(name="manualDeleteForm", extend = "adminTemplate", putAttributes = {
+    @TilesPutAttribute(name = "navigation", value = "/WEB-INF/tiles/active/navigation/others.jsp"),
+    @TilesPutAttribute(name = "content",    value = "/WEB-INF/tiles/active/view/manual/delete_form.jsp")
+  })
+})
 public class FormAction extends EmptyAction {
 
 	/**
