0c8061a4eeb628d1f170caa67dfa61a2ac8c2bc5
tiv-source <marc.michele@tiv-source.de>
2017-05-24 02:31:40 +0200

    Add interface Pagination to GalleryAction class of web-user project.

--- web-user/src/main/java/de/tivsource/page/user/actions/gallery/GalleryAction.java
+++ web-user/src/main/java/de/tivsource/page/user/actions/gallery/GalleryAction.java
@@ -17,8 +17,9 @@ import de.tivsource.page.dao.property.PropertyDaoLocal;
 import de.tivsource.page.entity.gallery.Gallery;
 import de.tivsource.page.entity.picture.Picture;
 import de.tivsource.page.user.actions.EmptyAction;
+import de.tivsource.page.user.interfaces.Pagination;
 
-public class GalleryAction extends EmptyAction {
+public class GalleryAction extends EmptyAction implements Pagination {
 
     /**
      * Serial Version UID.
@@ -47,7 +48,37 @@ public class GalleryAction extends EmptyAction {
 	 * Wird durch den Benutzer beeinflusst, es sollte vor der Verwendung auf
 	 * Sonderzeichen getestet werden.
 	 */
-    private String technical;
+    private String pathUuid;
+
+    /**
+     * Kann durch den Benutzer manipuliert werden, der wert sollte vor der
+     * Verwendung überprüft werden.
+     */ 
+    private Integer requestedPage;
+
+    /**
+     * Anzahl der Bilder aus der Datenbank ermittlet.
+     */
+    private Integer pictureCount;
+
+    /**
+     * Attribut das die maximal Anzahl der Liste enthält. 
+     */
+    private Integer maxElements = 5;
+
+    private Integer next;
+    private Integer previous;
+    private Integer current;
+
+    /**
+     * Attribut das den Startpunkt der Liste enthält.
+     */
+    private Integer from;
+
+    /**
+     * Attribute das die maximal mögliche Anzahl an Seiten enthält.
+     */
+    private Integer maxPages;
 
     @Override
     @Actions({
@@ -63,27 +94,44 @@ public class GalleryAction extends EmptyAction {
         // Hole Action Locale
         this.getLanguageFromActionContext();
 
-        technical = ServletActionContext.getRequest().getServletPath();
-        LOGGER.info("Technical: " + technical);
+        pathUuid = ServletActionContext.getRequest().getServletPath();
+        LOGGER.info("UUID from Path: " + pathUuid);
 
-        // /gallery/painting/index.html?page=1&request_locale=de
-        
-        
-        technical = technical.replaceAll("/index.html", "");
-        technical = technical.replaceAll("/gallery/", "");
+        // /gallery/index.html?page=1&request_locale=de
+        pathUuid = pathUuid.replaceAll("/index.html", "");
+        pathUuid = pathUuid.replaceAll("/gallery/", "");
             
-        LOGGER.info("Technical: " + technical);
+        LOGGER.info("UUID from Path: " + pathUuid);
 
         /*
-         * Wenn die Location Uuid keine nicht erlaubten Zeichen enthält und es
-         * die Location mit der Uuid gibt dann wird der Block ausgeführt.
+         * Wenn das Attribute pathUuid keine nicht erlaubten Zeichen enthält und
+         * es eine Galerie mit der Uuid gibt dann wird der Block ausgeführt.
          */
-        if (isValid(technical) && galleryDaoLocal.isGalleryTechnical(technical)) {
-            LOGGER.info("gültiger technischer Name.");
+        if (isValid(pathUuid) && galleryDaoLocal.isGallery(pathUuid)) {
+            LOGGER.info("gültige Galerie UUID.");
 
-            gallery = galleryDaoLocal.findByTechnical(technical);
+            // Lade das Galerie Objekt aus der Datenbank
+            gallery = galleryDaoLocal.findByUuid(pathUuid);
+
+            // Ermittle die Anzahl der Bilder in der Galerie
+            pictureCount = pictureDaoLocal.countAllInGallery(gallery.getUuid());
+            LOGGER.info("Inhalt Attribute pictureCount:" + pictureCount);
+
+            /*
+             * Ermittle Wert des Attributes maxElements aus Datenbank, versuche
+             * die Eigenschaft gallery.page.max.pictures zu laden.
+             */
+            if(propertyDaoLocal.findByKey("gallery.page.max.pictures") != null) {
+                maxElements = Integer.parseInt(propertyDaoLocal.findByKey("gallery.page.max.pictures").getValue());
+            }
+
+            // Kalkuliere Zähler Werte
+            calculate();
+
+            LOGGER.info("Inhalt Attribute from:" + from);
+
             // TODO: Neue Methode für die Abfrage
-            pictures = pictureDaoLocal.findAll(0, 10);
+            pictures = pictureDaoLocal.findAll(from, maxElements, gallery.getUuid(), "p.orderNumber, p.uuid", "desc");
 
             return SUCCESS;
         }
@@ -103,12 +151,63 @@ public class GalleryAction extends EmptyAction {
 		return pictures;
 	}
 
-	private Boolean isValid(String input) {
+    @Override
+    public Integer getNext() {
+        return next;
+    }
+
+    @Override
+    public Integer getPrevious() {
+        return previous;
+    }
+
+    @Override
+    public Integer getCurrent() {
+        return current;
+    }
+
+    @Override
+    public void setPage(Integer page) {
+        this.requestedPage = page;
+    }
+
+    private Boolean isValid(String input) {
         if (Pattern.matches("[abcdef0-9-]*", input)) {
             return true;
         } else {
             return false;
         }
     }
+
+    /**
+     * Methode die Start und Enpunkt der Liste und die vorherige beziehungweise
+     * die nächste Seitenzahl berechnet.
+     */
+    private void calculate() {
+        maxPages = (pictureCount % maxElements == 0) ? (pictureCount / maxElements)
+                : (pictureCount / maxElements) + 1;
+
+        // Wenn page nicht gesetzt wurde
+        if(requestedPage == null) {
+            requestedPage = 1;
+        }
+
+        //  Wenn page größer als maxPages ist.
+        if(requestedPage > maxPages) {
+            requestedPage = 1;
+        }
+        
+        if(requestedPage == 1) {
+            previous = null;
+            next = (2 <= maxPages) ? 2 : null;
+            from = 0;
+            current = 1;
+        } else {
+            previous = requestedPage -1;
+            next = (requestedPage + 1 <= maxPages) ? requestedPage + 1 : null;
+            from = (requestedPage - 1) * maxElements;
+            current = requestedPage;
+        }
+    }// Ende calculate()
 
 }// Ende class
