e01e2a245a203bacb77763527c72d14e66324c95
tiv-source <marc.michele@tiv-source.de>
2018-08-03 00:29:21 +0200

    Move jsp files of Manual class to new package.

--- web-user/src/main/java/de/tivsource/page/user/actions/manual/IndexAction.java
+++ web-user/src/main/java/de/tivsource/page/user/actions/manual/IndexAction.java
@@ -7,6 +7,9 @@ import org.apache.logging.log4j.Logger;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Actions;
 import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.tiles.annotation.TilesDefinition;
+import org.apache.struts2.tiles.annotation.TilesDefinitions;
+import org.apache.struts2.tiles.annotation.TilesPutAttribute;
 
 import de.tivsource.ejb3plugin.InjectEJB;
 import de.tivsource.page.dao.manual.ManualDaoLocal;
@@ -17,6 +20,16 @@ import de.tivsource.page.entity.page.Page;
 import de.tivsource.page.user.actions.EmptyAction;
 import de.tivsource.page.user.interfaces.Pagination;
 
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+@TilesDefinitions({
+  @TilesDefinition(name="manualList", extend = "userTemplate", putAttributes = {
+    @TilesPutAttribute(name = "content",    value = "/WEB-INF/tiles/active/view/manual/manual_list.jsp")
+  })
+})
 public class IndexAction extends EmptyAction implements Pagination {
 
     /**
--- web-user/src/main/java/de/tivsource/page/user/actions/manual/ManualAction.java
+++ web-user/src/main/java/de/tivsource/page/user/actions/manual/ManualAction.java
@@ -8,6 +8,9 @@ import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Actions;
 import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.tiles.annotation.TilesDefinition;
+import org.apache.struts2.tiles.annotation.TilesDefinitions;
+import org.apache.struts2.tiles.annotation.TilesPutAttribute;
 
 import de.tivsource.ejb3plugin.InjectEJB;
 import de.tivsource.page.dao.manual.ManualDaoLocal;
@@ -17,6 +20,18 @@ import de.tivsource.page.entity.manual.Manual;
 import de.tivsource.page.entity.page.Page;
 import de.tivsource.page.user.actions.EmptyAction;
 
+/**
+ * 
+ * @author Marc Michele
+ *
+ */
+@TilesDefinitions({
+  @TilesDefinition(name="manual", extend = "userTemplate", putAttributes = {
+    @TilesPutAttribute(name = "meta",    value = "/WEB-INF/tiles/active/meta/manual.jsp"),
+    @TilesPutAttribute(name = "twitter", value = "/WEB-INF/tiles/active/twitter/manual.jsp"),
+    @TilesPutAttribute(name = "content", value = "/WEB-INF/tiles/active/view/manual/manual.jsp")
+  })
+})
 public class ManualAction extends EmptyAction {
 
     /**
