babdeb6c11ac4c2996c71651d220835c1300982f
tiv-source <marc.michele@tiv-source.de>
2020-01-18 01:09:59 +0100

    Kommentar hinzugefügt und Logging Code.

--- converter/src/main/java/de/tivsource/page/converter/CSSGroupConverter.java
+++ converter/src/main/java/de/tivsource/page/converter/CSSGroupConverter.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.struts2.util.StrutsTypeConverter;
 
 import com.opensymphony.xwork2.conversion.TypeConversionException;
@@ -12,12 +14,20 @@ import com.opensymphony.xwork2.conversion.TypeConversionException;
 import de.tivsource.page.common.css.CSSGroup;
 import de.tivsource.page.dao.cssgroup.CSSGroupDaoLocal;
 
-
+/**
+ *
+ * @author Marc Michele
+ *
+ */
 public class CSSGroupConverter extends StrutsTypeConverter {
+
+    private static final Logger LOGGER = LogManager.getLogger(CSSGroupConverter.class);
 
     @SuppressWarnings("rawtypes")
 	@Override
     public Object convertFromString(Map context, String[] values, Class toClass) {
+
+        LOGGER.info("convertFromString(Map context, String[] values, Class toClass) aufgerufen.");
 
         if(values.length<=0) {
             throw new TypeConversionException("Kein gültigen CSS - Gruppe angegeben");
