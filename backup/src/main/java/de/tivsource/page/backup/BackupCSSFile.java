edc2edc0e63fb918286d074289a99431445029f7
tiv-source <marc.michele@tiv-source.de>
2020-05-18 19:39:18 +0200

    Code kommentiert und Style angepasst.

--- backup/src/main/java/de/tivsource/page/backup/BackupCSSFile.java
+++ backup/src/main/java/de/tivsource/page/backup/BackupCSSFile.java
@@ -15,22 +15,48 @@ import org.apache.logging.log4j.Logger;
 import de.tivsource.page.common.css.CSSFile;
 import de.tivsource.page.dao.cssfile.CSSFileDaoLocal;
 
+/**
+ * Klasse die dazu dient eine Datensicherung der benutzen CSS-Dateien und
+ * Objekte zu erstellen.
+ *
+ * @author Marc Michele
+ *
+ */
 public class BackupCSSFile {
 
     /**
      * Statischer Logger der Klasse.
      */
-    private static final Logger LOGGER = LogManager.getLogger(BackupCSSFile.class);
+    private static final Logger LOGGER = LogManager
+            .getLogger(BackupCSSFile.class);
 
+    /**
+     * DAO-Klasse für CSSFile Objekte.
+     */
     private CSSFileDaoLocal cssFileDaoLocal;
 
+    /**
+     * Dateien die in die Sicherung mit einbezogen werden sollen.
+     */
     private List<File> backupFiles = new ArrayList<File>();
 
-    public BackupCSSFile(CSSFileDaoLocal cssFileDaoLocal) {
+    /**
+     * Konstruktor der Sicherungsklasse, es muss lediglich die DAO-Klasse
+     * übergeben werden.
+     *
+     * @param cssFileDaoLocalInput - DAO Klasse für CSSFile Objekte
+     */
+    public BackupCSSFile(final CSSFileDaoLocal cssFileDaoLocalInput) {
         super();
-        this.cssFileDaoLocal = cssFileDaoLocal;
+        this.cssFileDaoLocal = cssFileDaoLocalInput;
     }
 
+    /**
+     * Methode die das Backup erzeugt.
+     *
+     * @return
+     * @throws IOException
+     */
     public List<File> getBackupFiles() throws IOException {
         LOGGER.info("getBackupFile() aufgerufen.");
 
@@ -42,7 +68,7 @@ public class BackupCSSFile {
         // Format Definition
         bufferedWriter.write("[Format Definition] => uuid|name|description|priority|path|created|modified|modifiedBy|modifiedAddress|version|");
 
-        Iterator<CSSFile> cssFileIterator = cssFileDaoLocal.findAll(0,cssFileDaoLocal.countAll()).iterator();
+        Iterator<CSSFile> cssFileIterator = cssFileDaoLocal.findAll(0, cssFileDaoLocal.countAll()).iterator();
         while (cssFileIterator.hasNext()) {
             CSSFile next = cssFileIterator.next();
             bufferedWriter.write("\n");
@@ -52,12 +78,18 @@ public class BackupCSSFile {
 
         bufferedWriter.close();
         fileWriter.close();
-        
+
         backupFiles.add(backupFile);
 
         return backupFiles;
     }
 
+    /**
+     * Methode zum konvertieren eines CSSFile Objekte in einen String.
+     * 
+     * @param next - CSSFile Objekt das konvertiert werden soll
+     * @return String - Der String der dem übergebenen CSSFile Objekt entspricht.
+     */
     private String convertToCsvLine(CSSFile next) {
         LOGGER.info("convertToCsvLine(CSSFile next) aufgerufen.");
 
@@ -101,7 +133,14 @@ public class BackupCSSFile {
         return nextString.toString();
     }
 
-    private void storeCSSFileInList(CSSFile next) {
+    /**
+     * Methode zum einfügen der CSS-Datei die zu dem in CSSFile Objekt gehört in
+     * die Liste der Dateien die gesichert werden sollen.
+     * 
+     * @param next - CSSFile Objekt von dem die CSS-Datei in die Liste eingefügt
+     *            werden soll
+     */
+    private void storeCSSFileInList(final CSSFile next) {
         File file = new File(next.getPath());
         backupFiles.add(file);
     }
