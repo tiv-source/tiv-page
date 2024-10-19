47256b625b46f59111a712ea9443f35facae100a
tiv-source <marc.michele@tiv-source.de>
2024-10-19 13:57:19 +0200

    Import der Abhängigkeiten der Converter auf Struts Version 7 korrigiert.

--- converter/src/main/java/de/tivsource/page/converter/CSSGroupConverter.java
+++ converter/src/main/java/de/tivsource/page/converter/CSSGroupConverter.java
@@ -7,9 +7,8 @@ import javax.naming.InitialContext;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.struts2.conversion.TypeConversionException;
 import org.apache.struts2.util.StrutsTypeConverter;
-
-import com.opensymphony.xwork2.conversion.TypeConversionException;
 
 import de.tivsource.page.common.css.CSSGroup;
 import de.tivsource.page.dao.cssgroup.CSSGroupDaoLocal;
