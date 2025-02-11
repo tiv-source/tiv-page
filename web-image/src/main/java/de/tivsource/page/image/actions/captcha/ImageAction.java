c032cb12dfe685b48c479099e05b5ab672384a2b
tiv-source <marc.michele@tiv-source.de>
2025-02-11 02:43:25 +0100

    Erste Version des Umbaus der Klasse Captcha.

--- web-image/src/main/java/de/tivsource/page/image/actions/captcha/ImageAction.java
+++ web-image/src/main/java/de/tivsource/page/image/actions/captcha/ImageAction.java
@@ -4,23 +4,22 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.struts2.ActionSupport;
 import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.action.ServletRequestAware;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Actions;
 import org.apache.struts2.convention.annotation.Result;
-import org.apache.struts2.interceptor.ServletRequestAware;
-
-import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.interceptor.parameter.StrutsParameter;
 
 import de.tivsource.ejb3plugin.InjectEJB;
 import de.tivsource.page.common.captcha.image.CaptchaImage;
 import de.tivsource.page.dao.captcha.CaptchaDaoLocal;
 import de.tivsource.page.dao.property.PropertyDaoLocal;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  *
@@ -56,10 +55,6 @@ public class ImageAction extends ActionSupport implements ServletRequestAware {
      */
     private boolean cache = true;
 
-    public void setServletRequest(HttpServletRequest servletRequest) {
-        this.servletRequest = servletRequest;
-    }
-
     /**
      * @return the captchaImage
      */
@@ -70,6 +65,7 @@ public class ImageAction extends ActionSupport implements ServletRequestAware {
     /**
      * @param cache the cache to set
      */
+    @StrutsParameter
     public void setCache(boolean cache) {
         this.cache = cache;
     }
@@ -208,6 +204,11 @@ public class ImageAction extends ActionSupport implements ServletRequestAware {
             exception.printStackTrace();
             return ERROR;
         }
+    }
+
+    @Override
+    public void withServletRequest(HttpServletRequest httpServletRequest) {
+        this.servletRequest = httpServletRequest;
     }
 
 }// Ende class
