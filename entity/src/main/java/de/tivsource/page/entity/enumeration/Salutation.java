041bd74c95f2612b6feb358d0e4ddd15558656a1
tiv-source <marc.michele@tiv-source.de>
2025-10-03 01:15:06 +0200

    Enumeration Salution hinzugefügt.

---
+++ entity/src/main/java/de/tivsource/page/entity/enumeration/Salutation.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package de.tivsource.page.entity.enumeration;
+
+/**
+ * @author Marc Michele
+ *
+ */
+public enum Salutation {
+    MALE,
+    FEMALE,
+    DIVERSE,
+    NONE;
+}// Ende enum
